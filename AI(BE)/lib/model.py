b2d3566b0a846463c1088f63b179096fa0d7500e
Jun0zo <joon0zo1022@gmail.com>
2022-09-22 00:17:21 +0000

    Uniform Checker class 단위로 분리

--- AI(BE)/lib/model.py
+++ AI(BE)/lib/model.py
@@ -1,19 +1,22 @@
-class He:
+from .classificate import classificate
+
+class OmilZomil:
     def __init__(self):
-        self.HED_engine = HED()
+        # self.HED_engine = HED()
+        print('init!')
         self.org = None
         self.gray = None
         self.edge = None
-
-    def detect(img):
+    
+    def detect(self, img):
         self.org = img
         # check_person(org) 사람인식
         # hair_segmentation(org) 머리카락인식
-        # kind = classification() # 복장종류인식 (전투복, 동정복, 샘당)
+        kind = classificate(self.org) # 복장종류인식 (전투복, 동정복, 샘당)
         # if kind == '1':
         #   
         # elif kind == '2':
         #   
         # elif kind == '3':
         #   
-        pass
+        return None
