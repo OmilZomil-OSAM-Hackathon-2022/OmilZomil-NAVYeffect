84fa93ac7ed2485144582eead1345b09ac0c50ed
biyotteu <tjqtjq0516@gmail.com>
2022-09-23 12:49:24 +0000

    axios 테스트

--- WEB_FE/omilzomil/src/main.js
+++ WEB_FE/omilzomil/src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+
+// Vue
 const app = createApp(App)
+app.config.globalProperties.$axios = axios.create({
+        timeout: 3000,
+        headers: {
+          // 헤더 세팅
+        },
+        proxy: {
+          // url 리소스를 추가해주자
+          "/user": {
+            // 해당 리소스가 있는 url일 경우 타겟으로 baseURL을 변경
+            target: "http://117.17.110.220:7717/user/",
+            // 기본 베이스URL을 바꿔줄지 여부
+            changeOrigin: true
+          },
+        }
+      })
 
 app.use(router).mount('#app')
