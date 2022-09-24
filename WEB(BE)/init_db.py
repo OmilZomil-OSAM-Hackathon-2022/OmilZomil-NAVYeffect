86489e783f41d1890325b30cb58ff8968ac18655
greenrain78 <kdwkdw0078@gmail.com>
2022-09-24 13:16:35 +0000

    db 생성 오류

--- WEB(BE)/init_db.py
+++ WEB(BE)/init_db.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from core.settings import DB_INFO
 
 db_url = 'postgresql://{user}:{pw}@{ip}:{port}/{name}'.format(**DB_INFO)
+print(db_url)
+
 engine = create_engine(db_url, echo=True)
 
 
