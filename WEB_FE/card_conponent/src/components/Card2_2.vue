7810e390938c527dae7a583637747151b65b4af6
HeoTaeRyang <cake0702@naver.com>
2022-09-24 04:26:35 +0000

    카드 컴포넌트 그림자 수정

--- WEB_FE/card_conponent/src/components/Card2_2.vue
+++ WEB_FE/card_conponent/src/components/Card2_2.vue
@@ -4,7 +4,6 @@
     width: 384px;
     height: 562px;
     background: #FFFFFF;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.05);
     border-radius: 20px;">
         <img src="@/assets/card22.svg" />
     </div>
--- WEB_FE/card_conponent/src/components/Card3_2.vue
+++ WEB_FE/card_conponent/src/components/Card3_2.vue
@@ -4,7 +4,6 @@
     width: 792px;
     height: 417px;
     background: #FFFFFF;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.05);
     border-radius: 20px;">
         <img src="@/assets/card32.svg" />
     </div>
