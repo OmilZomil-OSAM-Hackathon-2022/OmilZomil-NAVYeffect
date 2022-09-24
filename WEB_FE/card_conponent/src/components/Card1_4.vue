5276f57cfddaffb7c529422153ae23e9e50e16c7
HeoTaeRyang <cake0702@naver.com>
2022-09-24 04:14:39 +0000

    대시보드 임시 디자인

---
+++ WEB_FE/card_conponent/src/components/Card1_4.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="
+    position: absolute;
+    width: 180px;
+    height: 196px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.05);
+    border-radius: 20px;">
+        <img src="@/assets/card14.svg" />
+    </div>    
+</template>
---
+++ WEB_FE/card_conponent/src/components/Card4_1.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="
+    position: absolute;
+    width: 384px;
+    height: 196px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.05);
+    border-radius: 20px;">
+        <img src="@/assets/card41.svg" />
+    </div>
+</template>
