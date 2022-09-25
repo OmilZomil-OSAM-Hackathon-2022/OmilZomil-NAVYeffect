0c9877f4d6e5d90c7aa75187ba358d17b0690af6
greenrain78 <kdwkdw0078@gmail.com>
2022-09-25 01:05:15 +0000

    임시 db 서버 오라클

--- WEB_BE/core/db_info.py
+++ WEB_BE/core/db_info.py
@@ -6,5 +6,12 @@ DB_LIST = {
         'user' : 'user',
         'pw': '1234',
         'name': 'dress',
+    },
+    'oracle' : {
+        'ip': '131.186.17.227',
+        'port' : 5432,
+        'user' : 'user',
+        'pw': '1234',
+        'name': 'dress',
     }
 }
