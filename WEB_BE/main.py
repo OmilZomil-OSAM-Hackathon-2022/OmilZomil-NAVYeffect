f60141eecab13b5780de3833824d53918138dacd
greenrain78 <kdwkdw0078@gmail.com>
2022-09-26 22:40:02 +0900

    cors

--- WEB_BE/main.py
+++ WEB_BE/main.py
@@ -1,8 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.user import router as user_router
+from core.settings import CORS_ORIGINS
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router.router)
