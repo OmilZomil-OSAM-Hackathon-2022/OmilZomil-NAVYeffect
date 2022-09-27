54c3602d69ee387cea2a47a5d7017d61d5a3b568
Jun0zo <joon0zo1022@gmail.com>
2022-09-27 14:36:50 +0900

    Uniform Checker에 upper lower 필터추가

--- AI_BE/OZEngine/dress_checkers/FullDressUniformChecker.py
+++ AI_BE/OZEngine/dress_checkers/FullDressUniformChecker.py
@@ -4,10 +4,15 @@ from lib.defines import *
 from lib.ocr import OCR, draw_rectangle
 
 # (동)정복 검사
+
+
 class FullDressUniformChecker():
     def __init__(self):
         # hyperparameter
-        self.uniform_filter = {'lower': (12,0,0), 'upper': (197,255,116)}
+        self.uniform_filter = {'lower': (12, 0, 0), 'upper': (197, 255, 116)}
+        self.anchor_filter = {'lower': (20, 100, 100), 'upper': (30, 255, 255)}
+        self.classes_filter = {
+            'lower': (140, 120, 50), 'upper': (190, 255, 255)}
 
     def getName(self, org_img):
         img = org_img.copy()
@@ -29,12 +34,15 @@ class FullDressUniformChecker():
         hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         h, w = img.shape[:2]
 
-        lower, upper = self.uniform_filter['lower'], self.uniform_filter['upper'] # 정복 filter 
+        # 정복 filter
+        lower, upper = self.uniform_filter['lower'], self.uniform_filter['upper']
         black_mask = cv2.inRange(hsv_img, lower, upper)
         masked_img = cv2.bitwise_and(img, img, mask=black_mask)
 
-        contours, hierarchy = cv2.findContours(black_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        sorted_contours, sorted_hierarchy = sortContoursByArea(contours, hierarchy)
+        contours, hierarchy = cv2.findContours(
+            black_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        sorted_contours, sorted_hierarchy = sortContoursByArea(
+            contours, hierarchy)
 
         ocr_str, ocr_boxes = OCR(img)
         print('ocr:', ocr_str, ocr_boxes)
@@ -52,10 +60,12 @@ class FullDressUniformChecker():
                 continue
 
             # 정복 영영 안쪽 && 모서리가 4~5 && 크기가 {hyperParameter} 이상 => (이름표)
-            if parent == shirt_node and 4 <= getVertexCnt(contour) <= 5 and cv2.contourArea(contour) > 300: # 이름표 또는 계급장
+            # 이름표 또는 계급장
+            if parent == shirt_node and 4 <= getVertexCnt(contour) <= 5 and cv2.contourArea(contour) > 300:
                 center_p = getContourCenterPosition(contour)
-                max_xy, min_xy = np.max(contour, axis=0)[0],np.min(contour, axis=0)[0]
-                
+                max_xy, min_xy = np.max(contour, axis=0)[
+                    0], np.min(contour, axis=0)[0]
+
                 # 이름표 체크
                 if center_p[0] < (w//2) and not component_dic.get('name_tag'):
                     for ocr_box in ocr_boxes:
@@ -68,14 +78,15 @@ class FullDressUniformChecker():
                             contour_dic['name_tag'] = contour
                             component_dic['name_tag'] = name
                             cv2.drawContours(img, [contour], 0, Color.RED, 2)
-                            cv2.drawContours(img, [contour], 0, Color.GREEN, -1)
+                            cv2.drawContours(
+                                img, [contour], 0, Color.GREEN, -1)
                             drawPoint(img, center_p, Color.PURPLE, 50)
 
         half_line_p1, half_line_p2 = (w//2, 0), (w//2, h)
         cv2.line(img, half_line_p1, half_line_p2, Color.WHITE, 5)
 
-        
         cv2.imwrite('./res/res05.jpg', masked_img)
         cv2.imwrite('./res/res06.jpg', img)
-        plt_imshow(['black_mask', 'masked img (bitwise and)', 'img'], [black_mask, masked_img, img])
+        plt_imshow(['black_mask', 'masked img (bitwise and)', 'img'], [
+                   black_mask, masked_img, img])
         return component_dic, contour_dic
