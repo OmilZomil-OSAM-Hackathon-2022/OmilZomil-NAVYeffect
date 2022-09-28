c33d62b94d2f5bad2e1b49d03144c2d567b59cf7
greenrain78 <kdwkdw0078@gmail.com>
2022-09-28 20:34:57 +0900

    include token router

--- WEB_BE/main.py
+++ WEB_BE/main.py
@@ -23,8 +23,10 @@ app.add_middleware(
 
 from app.user import router as user_router
 from app.test import router as test_router
+from app.token import router as token_router
 
 
 
 app.include_router(user_router.router)
+app.include_router(token_router.router)
 app.include_router(test_router.router)
