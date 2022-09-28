9f9d228d89d5820de289d81b7a00f1a46dabb295
greenrain78 <kdwkdw0078@gmail.com>
2022-09-28 12:04:27 +0900

    user 설명

--- WEB_BE/app/user/schema.py
+++ WEB_BE/app/user/schema.py
@@ -7,12 +7,12 @@ from core.base_schema import AllOptional
 # Shared properties
 class UserBase(BaseModel):
     name: str = Field(title="성함", description='사용자 성함')
-    uid: str
-    password: str
-    dog_num: str
-    army: str
-    unit: str
-    rank: str
+    uid: str = Field(title="아이디", description='사용자 아이디 모두 유니크')
+    password: str = Field(title="패스워드", description='비번은 알아서 잘 제한 없음')
+    dog_num: str = Field(title="군번", description='군번은 태그가서있어서 문자열로 저장')
+    army: str = Field(title="육해공군", description='육군 or 해군 or 공군 or 국직')
+    unit: str = Field(title="소속", description='소속 부대 그냥 ipscan처럼 그대로 저장')
+    rank: str = Field(title="계급", description='계급')
 
 
 # Properties to receive on item creation
