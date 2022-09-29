adf0d03c802016f8e67be34a0ef6cb4875cd7aef
greenrain78 <kdwkdw0078@gmail.com>
2022-09-29 21:49:33 +0900

    camera router create

--- WEB_BE/app/camera/model.py
+++ WEB_BE/app/camera/model.py
@@ -3,7 +3,7 @@ from sqlalchemy import Boolean, Column, String, Text, Integer
 from core.db import Base
 
 class Camera(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'camera'
 
     id = Column(Integer, primary_key=True, index=True)
     uid = Column(String, unique=True)
