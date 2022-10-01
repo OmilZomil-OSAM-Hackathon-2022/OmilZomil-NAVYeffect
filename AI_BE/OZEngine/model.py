dab3ea3a4b3bd467e9e5cf27e6796fc1093b1487
Jun0zo <joon0zo1022@gmail.com>
2022-10-01 16:25:41 +0900

    debug_dic, ocr_img 제거

--- AI_BE/OZEngine/model.py
+++ AI_BE/OZEngine/model.py
@@ -27,50 +27,49 @@ class OmilZomil:
         plt_imshow(['morphed', 'hed'], [morphed_edge, hed_edge])
 
     def debug(self, debug_img):
-        names, imgs= list(debug_img.keys()), list(debug_img.values())
+        names, imgs = list(debug_img.keys()), list(debug_img.values())
         plt_imshow(names, imgs)
 
     def contour2img(self, org_img, contour_dic):
-        img= org_img.copy()
-        roi_dic= {}
+        img = org_img.copy()
+        roi_dic = {}
 
         # cv2.drawContours(img, [contour_dic['shirt']], 0, Color.GREEN, -1)
         for name, contour in contour_dic.items():
             if name != 'shirt' and contour is not None:
-                x, y, w, h= cv2.boundingRect(contour_dic[name])
-                roi= org_img[y:y+h, x:x+w]
+                x, y, w, h = cv2.boundingRect(contour_dic[name])
+                roi = org_img[y:y+h, x:x+w]
                 cv2.rectangle(img, (x, y), (x+w, y+h), Color.PURPLE, 5)
-                font= cv2.FONT_HERSHEY_SIMPLEX
+                font = cv2.FONT_HERSHEY_SIMPLEX
                 cv2.putText(img, name, (x, y), font, 3, Color.PURPLE, 5)
-                roi_dic[name]= roi
+                roi_dic[name] = roi
 
         return img, roi_dic
 
     def detect(self, img):
-        person_roi= None
+        input_img = None
         if self.detect_person:
-            person_roi, boxed_img= self.person_detector.detect(img)  # 사람인식
-
-        if person_roi is None:
-            raise Exception("인식가능한 사람이 없습니다!")
+            input_img, boxed_img = self.person_detector.detect(input_img)  # 사람인식
+            if person_roi is None:
+                raise Exception("인식가능한 사람이 없습니다!")
+        else:
+            input_img = img
 
         # hair_segmentation(org) 머리카락인식
-        # kind = classificate(self.org)  # 복장종류인식 (전투복, 동정복, 샘당)
 
-        # self.kind = UniformType.dic['FULL_DRESS']
-        self.kind= UniformType.dic['FULL_DRESS']
+        self.kind = UniformType.dic['FULL_DRESS']
+
+        if self.kind is None:
+            self.kind = classificate(self.org)  # 복장종류인식 (전투복, 동정복, 샘당)
 
         if self.kind == UniformType.dic['NAVY_SERVICE']:
-            component_dic, contour_dic, debug_img = self.navy_service_uniform_checker.checkUniform(
-                person_roi)
+            component_dic, contour_dic, masked_img = self.navy_service_uniform_checker.checkUniform(
+                input_img)
 
         elif self.kind == UniformType.dic['FULL_DRESS']:
-            component_dic, contour_dic, debug_img, masked_img = self.full_dress_uniform_checker.checkUniform(
-                person_roi)
+            component_dic, contour_dic, masked_img = self.full_dress_uniform_checker.checkUniform(
+                input_img)
 
-        if self.detect_person:
-            boxed_img, roi_dic= self.contour2img(person_roi, contour_dic)
-        else:
-            boxed_img, roi_dic= self.contour2img(img, contour_dic)
+        # boxed_img, roi_dic= self.contour2img(input_img, contour_dic)
 
         return component_dic, boxed_img, roi_dic, masked_img
