7c1d1d926eec947cc965fe9e6662a890ad0917c7
biyotteu <tjqtjq0516@gmail.com>
2022-10-02 15:18:25 +0900

    앱바 아이콘 액티브 제작

--- WEB_FE/omilzomil/src/components/IconBase.vue
+++ WEB_FE/omilzomil/src/components/IconBase.vue
@@ -3,7 +3,7 @@
       xmlns="http://www.w3.org/2000/svg"
       :width="width"
       :height="height"
-      viewBox="0 0 18 18"
+      viewBox="0 0 24 24"
       :aria-labelledby="iconName"
       role="presentation"
     >
