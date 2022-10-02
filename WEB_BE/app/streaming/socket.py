2dca7a0db540a4e7e11e604b2996d6e5501b9ddc
greenrain78 <kdwkdw0078@gmail.com>
2022-10-02 13:05:29 +0900

    web socket 임시저장

--- WEB_BE/app/streaming/socket.py
+++ WEB_BE/app/streaming/socket.py
@@ -1,22 +1,26 @@
-from typing import List
+from typing import List, Dict
 
 from fastapi.websockets import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: Dict[WebSocket] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, str: name,  websocket: WebSocket):
+        if name in self.active_connections:
+            return False
+        
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[name] =(websocket)
+        return True
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, name: str):
+        self.active_connections.pop(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for name, connection in self.active_connections.items():
             await connection.send_text(message)
