ad1573469284f21fdd1865cece31f05c16d68116
greenrain78 <kdwkdw0078@gmail.com>
2022-10-03 00:07:43 +0900

    build 시 pem 파일 생성

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 echo [+] makeing DB 
+
+# docker 빌드
+sudo docker-compose build
+
 # DB 실행
 sudo docker-compose up -d db
 
 # DB 테이블 만들기
-sudo docker-compose run backend python src/init_db.py
+sudo docker-compose run backend python backend/src/init_db.py
+
+# ssl 만들기
+cd ./backend/deploy
+sh make_ssl.sh 
