e7e8a11af85f914e96982f1ddee8b81a6d8fbc6b
greenrain78 <kdwkdw0078@gmail.com>
2022-10-02 19:25:47 +0900

    frontend docker file 작성

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,1 @@
-# python main.py
-pytest
-# sh docker/make_ssl.sh --  안됨
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ssl-keyfile=./docker/key.pem --ssl-certfile=./docker/cert.pem
-# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+npm 
