5f741fd337a068a136ad0d0219e95c64211b87fe
greenrain78 <kdwkdw0078@gmail.com>
2022-10-02 19:51:40 +0900

    db info 제거

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -1,9 +1,15 @@
 import os 
 from core.db_info import DB_LIST
 
-DB_NAME = os.environ['DB_NAME']
+DB_NAME = "MYSQL"
 
-DB_INFO = DB_LIST[DB_NAME]
+DB_INFO = {
+        'ip': f"{DB_NAME}_{os.environ['DB_NAME']}",
+        'port' : f"{DB_NAME}_{os.environ['DB_NAME']}",
+        'user' : f"{DB_NAME}_{os.environ['DB_NAME']}",
+        'pw': f"{DB_NAME}_{os.environ['DB_NAME']}",
+        'name': f"{DB_NAME}_{os.environ['DB_NAME']}",
+    },
 
 
 CORS_ORIGINS = [
