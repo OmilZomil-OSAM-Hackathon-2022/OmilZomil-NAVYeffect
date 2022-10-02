3aab2a79b1181b3cb2850b49e6ab8653997eadbf
greenrain78 <kdwkdw0078@gmail.com>
2022-10-02 19:59:40 +0900

    mysql 접속 포트 변경

--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -3,7 +3,7 @@ import os
 
 DB_INFO = {
         'ip': os.environ['MYSQL_HOST'],
-        'port' : 33060,
+        'port' : 3306,
         'user' : os.environ['MYSQL_USER'],
         'pw': os.environ['MYSQL_PASSWORD'],
         'name': os.environ['MYSQL_DB'],
