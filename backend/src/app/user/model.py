c0782cfc307863e481d7a6e010d283165aa2b461
greenrain78 <kdwkdw0078@gmail.com>
2022-10-02 20:31:07 +0900

    mysql은 string 제한 설정 있어야함

--- backend/src/app/user/model.py
+++ backend/src/app/user/model.py
@@ -6,11 +6,11 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)   # 성명
-    uid = Column(String)
-    password = Column(String)
-    army = Column(String)  # 육해공군
-    unit = Column(String)   # 소속
-    dog_num = Column(String) # 군번
-    rank = Column(String)    # 계급
+    name = Column(String(30))   # 성명
+    uid = Column(String(50))
+    password = Column(String(300))
+    army = Column(String(50))  # 육해공군
+    unit = Column(String(50))   # 소속
+    dog_num = Column(String(50)) # 군번
+    rank = Column(String(50))    # 계급
     permission = Column(Integer)# 권한
