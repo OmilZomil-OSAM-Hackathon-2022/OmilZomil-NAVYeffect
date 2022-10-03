937bcbb2be53b82841ef206a46eea646ecab0be2
greenrain78 <kdwkdw0078@gmail.com>
2022-10-03 12:36:09 +0900

    build 시에도 env.privare 읽기

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 echo [+] makeing DB 
 
 # docker 빌드
-sudo docker-compose -f docker-compose.server.yml build
+sudo docker-compose -f docker-compose.server.yml --env-file .env.private build
 
 # DB 실행
-sudo docker-compose -f docker-compose.server.yml up -d db
+sudo docker-compose -f docker-compose.server.yml --env-file .env.private up -d db
 
 # DB 테이블 만들기
-sudo docker-compose -f docker-compose.server.yml run backend python src/init_db.py
+sudo docker-compose -f docker-compose.server.yml --env-file .env.private run backend python src/init_db.py
 
 # ssl 만들기
 cd ./backend/deploy
