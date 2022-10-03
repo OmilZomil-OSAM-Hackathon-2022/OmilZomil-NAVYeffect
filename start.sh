a017e3097b45b4d98f507a57585746071fc7dd69
greenrain78 <kdwkdw0078@gmail.com>
2022-10-03 18:29:30 +0900

    .env.lock 파일 작성

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #/bin/bash
 
+echo [+] Checking build files...
+if [ -f .env.lock ] ; then
+    :
+else
+    echo "[!] Please run 'build.sh' first!"
+    exit
+fi
+
 echo [+] run web
-sudo docker-compose --env-file .env.private up
+sudo docker-compose --env-file .env.lock up
--- stop.sh
+++ stop.sh
@@ -2,4 +2,4 @@
 
 echo [+] Stopping docker container
 
-sudo docker-compose --env-file .env.private stop frontend backend
+sudo docker-compose --env-file .env.lock stop frontend backend
