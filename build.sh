31fe0b8690b3493af32e3d2b7d72757d57f136f8
greenrain78 <kdwkdw0078@gmail.com>
2022-10-03 11:47:29 +0900

    docker compose.server 로 변경

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 echo [+] makeing DB 
 
 # docker 빌드
-sudo docker-compose build
+sudo docker-compose -f docker-compose.server.yml build
 
 # DB 실행
-sudo docker-compose up -d db
+sudo docker-compose -f docker-compose.server.yml up -d db
 
 # DB 테이블 만들기
-sudo docker-compose run backend python backend/src/init_db.py
+sudo docker-compose -f docker-compose.server.yml run backend python backend/src/init_db.py
 
 # ssl 만들기
 cd ./backend/deploy
 sh make_ssl.sh 
 
-sudo docker container prune
+# docker 빌드 캐쉬 제거
+sudo docker builder prune
