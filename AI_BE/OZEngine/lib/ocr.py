60da72634247e445b5ab8e68822e6141d998d2a6
Jun0zo <joon0zo1022@gmail.com>
2022-10-03 14:37:15 +0900

    OZEngine 복구

---
+++ AI_BE/OZEngine/lib/ocr.py
@@ -0,0 +1,61 @@
+from lib.defines import *
+from sys import path as syspath
+from os.path import abspath, join, dirname
+import numpy as np
+import cv2
+import requests
+
+syspath.append(abspath(join(dirname(__file__), '../../lib/')))
+# from lib.utils import *
+
+
+print('OCR loaded!')
+API_URL = 'https://dapi.kakao.com/v2/vision/text/ocr'
+KEY = '5b60f7aeb82db9bf02f3eea5a94a69c0'
+
+
+def str_encoder(str):
+    str = str.replace('\\', "#'W'#").replace('/', "#'R'#").replace(':',
+                                                                   "#'C'#").replace('*', "#'S'#").replace('?', "#'Q'#").replace('"', "#'d'#")
+    str = str.replace('<', "#'L'#").replace(
+        '>', "#'G'#").replace('/', "#'O'#").replace('.', "#'E'#")
+    return str
+
+
+def str_decoder(str):
+    str = str.replace("#'W'#", '\\').replace("#'R'#", '/').replace("#'C'#",
+                                                                   ':').replace("#'S'#", '*').replace("#'Q'#", '?').replace("#'D'#", '"')
+    str = str.replace("#'L'#", '<').replace(
+        "#'G'#", '>').replace("#'O'#", '/').replace("#'E'#", '.')
+    return str
+
+
+def draw_rectangle(image, p1, p3, color, border, padding):
+    pp1 = (p1[0]-padding, p1[1]-padding)
+    pp2 = (p3[0]+padding, p3[1]+padding)
+    cv2.rectangle(image, pp1, pp2, color, border)
+
+
+def OCR(org_img):
+    img = org_img.copy()
+    headers = {'Authorization': 'KakaoAK {}'.format(KEY)}
+    jpeg_img = cv2.imencode(".jpg", img)[1]
+    data = jpeg_img.tobytes()
+    ocr_json = requests.post(API_URL, headers=headers, files={"image": data})
+    
+    outputs = ocr_json.json()['result']
+
+
+    return outputs
+
+    # for output in outputs:
+    #     padding = 10  # 10
+    #     p1, p2, p3, p4 = output['boxes']  # LU RU RD LD
+    #     boxes.append((p1, p2, p3, p4))
+    #     #
+
+    #     if not output['recognition_words'] == '':
+    #         res += " ".join(output['recognition_words']) + " "
+
+    # res = res.strip().replace(' ', '_')
+    # return str_encoder(res), boxes
