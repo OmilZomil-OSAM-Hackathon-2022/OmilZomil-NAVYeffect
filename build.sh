afd632a616d36d0b8b37e473494e9418f4f1d21e
greenrain78 <kdwkdw0078@gmail.com>
2022-10-03 12:04:28 +0900

    docker 백앤드 작업 폴더 변경

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ sudo docker-compose -f docker-compose.server.yml build
 sudo docker-compose -f docker-compose.server.yml up -d db
 
 # DB 테이블 만들기
-sudo docker-compose -f docker-compose.server.yml run backend python backend/src/init_db.py
+sudo docker-compose -f docker-compose.server.yml run backend python src/init_db.py
 
 # ssl 만들기
 cd ./backend/deploy
