465eda95087f3125e59c641a0eaccd6a633bd790
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-04 13:00:45 -0400

    부대 출입 정보 schema에 NOT NULL constraint 추가

--- backend/src/app/base_access/model.py
+++ backend/src/app/base_access/model.py
@@ -5,7 +5,7 @@ from core.db import Base
 class BaseAccess(Base):
     __tablename__ = "base_access"
 
-    access_id = Column(Integer, primary_key=True, index=True)
-    base_name = Column(String(30))  # 부대명
-    access_time = Column(DateTime)  # 출입 시간
-    image = Column(LargeBinary)  # 출입 사진
+    access_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    base_name = Column(String(30), nullable=False)  # 부대명
+    access_time = Column(DateTime, nullable=False)  # 출입 시간
+    image = Column(LargeBinary, nullable=False)  # 출입 사진
