d64cff621ed4b72bbe4b41ed7fe2e6959f35bada
Jun0zo <joon0zo1022@gmail.com>
2022-10-05 00:45:31 +0900

    video2image 함수에서 rotate기능 추가

--- AI_BE/demo/preprocess/video2image.py
+++ AI_BE/demo/preprocess/video2image.py
@@ -2,18 +2,39 @@ import cv2
 import time
 
 
-def video2image(video, fps):
-    prev_time = 0
-    frame_cnt = 0
+def video2image(video):
+    i = 0
     while True:
         ret, frame = video.read()
-        current_time = time.time() - prev_time
-        if (ret is True) and (current_time > 1. / fps):
-            prev_time = time.time()
-            cv2.imwrite(f'../image/video_result/1/{frame_cnt}.jpg', frame)
+        if ret:
+            print(frame.shape, i)
+            h, w, c = frame.shape
+            if w > h:
+                ro = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+                frame = ro
+                # ro1 = cv2.getRotationMatrix2D((w/2, h/2), -90, 1)
+                # tr1 = cv2.warpAffine(frame, ro1, (w, h))
+                # tr1 = cv2.resize(tr1, dsize=(
+                #     h, w), interpolation=cv2.INTER_AREA)
+                # frame = tr1
+
+            print(frame.shape, i)
+            cv2.imwrite(f'../image/video_frame/1/{i}.jpg', frame)
+            i += 1
+            break
         else:
             break
+    print(i)
 
-FPS = 1
+
 video = cv2.VideoCapture('../video/1.mp4')
-video2image(video, FPS)
+# print('fps', video.get(cv2.CAP_PROP_FPS))
+# width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+# height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# print('original size: %d, %d' % (width, height))
+
+# video.set(cv2.CAP_PROP_FRAME_WIDTH, height)
+# video.set(cv2.CAP_PROP_FRAME_HEIGHT, width)
+# print('original size: %d, %d' % (width, height))
+
+video2image(video)
