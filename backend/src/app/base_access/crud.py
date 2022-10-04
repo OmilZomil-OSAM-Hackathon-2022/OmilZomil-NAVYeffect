99754cd82279cb3d56323ead44eebc4d00cc609e
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-04 12:46:08 -0400

    부대 출입 정보 create 변수명 변경

--- backend/src/app/base_access/crud.py
+++ backend/src/app/base_access/crud.py
@@ -7,8 +7,8 @@ def get_access_by_id(db: Session, access_id: int):
     return db.query(BaseAccess).filter(BaseAccess.access_id == access_id).first()
 
 
-def create_access(db: Session, bac: BaseAccessCreate):
-    base_access = BaseAccess(base_name=bac.base_name, access_time=bac.access_time, image=bac.image)
+def create_access(db: Session, access: BaseAccessCreate):
+    base_access = BaseAccess(base_name=access.base_name, access_time=access.access_time, image=access.image)
     db.add(base_access)
     db.commit()
     db.refresh(base_access)
