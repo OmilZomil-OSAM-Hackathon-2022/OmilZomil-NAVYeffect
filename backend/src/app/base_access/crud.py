7378ac02e2d95103ae66aa6df273c49efe2657c2
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-04 12:30:51 -0400

    부대 출입 정보 read 함수명 오류 해결

--- backend/src/app/base_access/crud.py
+++ backend/src/app/base_access/crud.py
@@ -7,7 +7,7 @@ def get_access_by_id(db: Session, access_id: int):
     return db.query(BaseAccess).filter(BaseAccess.access_id == access_id).first()
 
 
-def create_user(db: Session, bac: BaseAccessCreate):
+def create_access(db: Session, bac: BaseAccessCreate):
     base_access = BaseAccess(base_name=bac.base_name, access_time=bac.access_time, image=bac.image)
     db.add(base_access)
     db.commit()
