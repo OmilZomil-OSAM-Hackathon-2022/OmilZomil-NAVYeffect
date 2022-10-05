b77e167d90aefeab6bb404f85ea482821b581fd7
Jun0zo <joon0zo1022@gmail.com>
2022-10-05 14:26:16 +0900

    디렉토리 명 변경 (AI_BE -> ai)

--- AI_BE/demo/preprocess/image_preprocess.py
+++
@@ -1,8 +0,0 @@
-import os
-path = './image/navy_service_uniform/person'
-
-
-for index, file_name in enumerate(os.listdir(path)):
-    src_file_path = os.path.join(path, file_name)
-    dst_file_path = os.path.join(path, f'{index}.jpg')
-    os.rename(src_file_path, dst_file_path)
