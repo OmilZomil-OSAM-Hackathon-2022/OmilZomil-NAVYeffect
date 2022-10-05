ff5f39be0db6f0241699ef2f94aa0a070dcda930
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-05 11:32:01 -0400

    crud 모듈 생성

--- backend/src/app/crud/enlisted_personnel.py
+++ backend/src/app/crud/enlisted_personnel.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
-from app.enlisted_personnel.model import EnlistedPersonnel
-from app.enlisted_personnel.schema import EnlistedPersonnelCreate, EnlistedPersonnelUpdate
+from app.models.enlisted_personnel import EnlistedPersonnel
+from app.schemas.enlisted_personnel import EnlistedPersonnelCreate, EnlistedPersonnelUpdate
 
 
-def get_personnel_by_id(db: Session, personnel_id: int):
+def get_enlisted_personnel_by_id(db: Session, personnel_id: int):
     return db.query(EnlistedPersonnel).get(personnel_id)
 
 
-def create_personnel(db: Session, personnel: EnlistedPersonnelCreate):
+def create_enlisted_personnel(db: Session, personnel: EnlistedPersonnelCreate):
     personnel = EnlistedPersonnel(
         access_id=personnel.access_id,
         army_type=personnel.army_type,
@@ -26,7 +26,7 @@ def create_personnel(db: Session, personnel: EnlistedPersonnelCreate):
     return personnel
 
 
-def update_personnel(db: Session, personnel_id: int, personnel: EnlistedPersonnelUpdate):
+def update_enlisted_personnel(db: Session, personnel_id: int, personnel: EnlistedPersonnelUpdate):
     personnel = {x: y for x, y in personnel.dict().items() if y is not None}
     db.query(EnlistedPersonnel).filter_by(personnel_id=personnel_id).update(personnel)
     db.commit()
