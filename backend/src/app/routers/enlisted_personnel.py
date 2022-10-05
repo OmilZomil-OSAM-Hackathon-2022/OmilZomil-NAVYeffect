d3cf8b3977838f188b4f8b7d944d4e6af4f862f9
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-05 11:33:50 -0400

    라우터 모듈 생성

--- backend/src/app/routers/enlisted_personnel.py
+++ backend/src/app/routers/enlisted_personnel.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 from core.db import get_db
-from app.enlisted_personnel.schema import EnlistedPersonnelUpdate
-from app.enlisted_personnel import crud
+import app.schemas.enlisted_personnel as schema
+import app.crud.enlisted_personnel as crud
 
 
 router = APIRouter(
@@ -13,11 +13,11 @@ router = APIRouter(
 
 @router.get("/{personnel_id}")
 async def get_enlisted_personnel_by_id(personnel_id: int, db: Session = Depends(get_db)):
-    return crud.get_personnel_by_id(db, personnel_id)
+    return crud.get_enlisted_personnel_by_id(db, personnel_id)
 
 
 @router.put("/{personnel_id}")
-async def update_enlisted_personnel(personnel_id: int, personnel: EnlistedPersonnelUpdate = Body(), db: Session = Depends(get_db)):
-    crud.update_personnel(db, personnel_id, personnel)
+async def update_enlisted_personnel(personnel_id: int, personnel: schema.EnlistedPersonnelUpdate = Body(), db: Session = Depends(get_db)):
+    crud.update_enlisted_personnel(db, personnel_id, personnel)
     res = {"success": True, "message": "Personnel info successfully updated"}
     return res
