2f323eee93506df257627e2379fb129a4448288f
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-05 11:52:37 -0400

    디렉토리 구조 변경 및 미완성 파일 삭제에 맞춘 init_db 및 main 변경

--- backend/src/init_db.py
+++ backend/src/init_db.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
 from core.settings import DB_INFO
-from app.user.model import Base as user_model
-from app.camera.model import Base as camera_model
-from app.base_access.model import Base as base_access_model
-from app.enlisted_personnel.model import Base as enlisted_personnel_model
+from app.models.base_access import Base as base_access_model
+from app.models.enlisted_personnel import Base as enlisted_personnel_model
 
 
 db_url = "mysql+mysqldb://{user}:{pw}@{ip}:{port}/{name}".format(**DB_INFO)
 print(db_url)
 
 engine = create_engine(db_url, echo=True)
-user_model.metadata.create_all(bind=engine)
-camera_model.metadata.create_all(bind=engine)
 base_access_model.metadata.create_all(bind=engine)
 enlisted_personnel_model.metadata.create_all(bind=engine)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,33 +1,6 @@
-import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-
-
-from core.settings import CORS_ORIGINS
+import app.routers.enlisted_personnel as enlisted_personnel_router
 
 
 app = FastAPI()
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-from app.user import router as user_router
-from app.test import router as test_router
-from app.token import router as token_router
-from app.camera import router as camera_router
-
-
-app.include_router(user_router.router)
-app.include_router(token_router.router)
-app.include_router(test_router.router)
-app.include_router(camera_router.router)
+app.include_router(enlisted_personnel_router.router)
