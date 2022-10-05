5865ca14277c9d5ec4573611c72c6ec89016e408
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-05 01:13:19 -0400

    장병 테이블 DB 초기 설정

--- backend/src/init_db.py
+++ backend/src/init_db.py
@@ -3,6 +3,8 @@ from core.settings import DB_INFO
 from app.user.model import Base as user_model
 from app.camera.model import Base as camera_model
 from app.base_access.model import Base as base_access_model
+from app.enlisted_personnel.model import Base as enlisted_personnel_model
+
 
 db_url = "mysql+mysqldb://{user}:{pw}@{ip}:{port}/{name}".format(**DB_INFO)
 print(db_url)
@@ -11,3 +13,4 @@ engine = create_engine(db_url, echo=True)
 user_model.metadata.create_all(bind=engine)
 camera_model.metadata.create_all(bind=engine)
 base_access_model.metadata.create_all(bind=engine)
+enlisted_personnel_model.metadata.create_all(bind=engine)
