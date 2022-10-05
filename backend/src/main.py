d04146ec6eede413ee4d920a943d9edb49651e95
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-05 04:14:26 -0400

    Revert "init_db 불필요 출력 삭제"
    This reverts commit 1bbc9c9afd0ba449d4540c0a22c5387cb7f48dee.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,18 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+
+
 from core.settings import CORS_ORIGINS
-from app.user import router as user_router
-from app.test import router as test_router
-from app.token import router as token_router
-from app.camera import router as camera_router
-from app.enlisted_personnel import router as enlisted_personnel_router
-from app.base_access import router as base_access_router
+
 
 app = FastAPI()
+
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
@@ -17,15 +21,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from app.user import router as user_router
+from app.test import router as test_router
+from app.token import router as token_router
+from app.camera import router as camera_router
+
 
 app.include_router(user_router.router)
 app.include_router(token_router.router)
 app.include_router(test_router.router)
 app.include_router(camera_router.router)
-app.include_router(enlisted_personnel_router.router)
-app.include_router(base_access_router.router)
-
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
