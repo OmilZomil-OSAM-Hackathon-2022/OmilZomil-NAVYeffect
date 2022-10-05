a53daa2c339ac8d5ed3ec822de097f999433e8b5
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-05 01:26:24 -0400

    장병 CRUD create 완성

---
+++ backend/src/app/enlisted_personnel/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from app.enslited_personnel.model import EnlistedPersonnel
+from app.enslited_personnel.schema import EnlistedPersonnelCreate
+
+
+def get_personnel_by_id(db: Session, personnel_id: int):
+    return db.query(EnlistedPersonnel).get(personnel_id)
+
+
+def create_personnel(db: Session, personnel: EnlistedPersonnelCreate):
+    personnel = EnlistedPersonnel(
+        access_id=personnel.access_id,
+        army_type=personnel.army_type,
+        name=personnel.name,
+        rank=personnel.rank,
+        uniform_type=personnel.uniform_type,
+        has_name=personnel.has_name,
+        has_rank=personnel.has_rank,
+        has_neckerchief=personnel.has_neckerchief,
+        has_muffler=personnel.has_muffler,
+        has_flag=personnel.has_flag,
+    )
+    db.add(personnel)
+    db.commit()
+    db.refresh(personnel)
+    return personnel
