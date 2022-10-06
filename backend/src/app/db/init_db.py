10d112f190f6868fda01675cf8fee9e9a747adb3
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-06 10:04:48 -0400

    init_db session 생성 부분 삭제

--- backend/src/app/db/init_db.py
+++ backend/src/app/db/init_db.py
@@ -1,11 +1,7 @@
-from sqlalchemy import create_engine
-from app.core.settings import DB_INFO
+from app.db.session import engine
 from app.models.base_access import Base as base_access_model
 from app.models.enlisted_personnel import Base as enlisted_personnel_model
 
-
-db_url = "mysql+mysqldb://{user}:{pw}@{ip}:{port}/{name}".format(**DB_INFO)
-engine = create_engine(db_url, echo=True)
 
 base_access_model.metadata.create_all(bind=engine)
 enlisted_personnel_model.metadata.create_all(bind=engine)
