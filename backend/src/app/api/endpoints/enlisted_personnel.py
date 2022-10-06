d55f5aaf883fd6e31dc0eaed6d672a03b7345ab3
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-06 10:38:02 -0400

    enlisted_personnel init.py 미사용

--- backend/src/app/api/endpoints/enlisted_personnel.py
+++ backend/src/app/api/endpoints/enlisted_personnel.py
@@ -1,25 +1,26 @@
 from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
-from app import crud, schemas
+from app.crud import enlisted_personnel as crud
+from app.schemas import enlisted_personnel as schema
 from app.api import deps
 
 
 router = APIRouter()
 
 
-@router.get("/{personnel_id}", response_model=schemas.enlisted_personnel.EnlistedPersonnelRead)
+@router.get("/{personnel_id}", response_model=schema.EnlistedPersonnelRead)
 async def get_personnel_info_by_id(personnel_id: int, db: Session = Depends(deps.get_db)):
-    personnel_info = crud.enlisted_personnel.get_personnel_info_by_id(db, personnel_id)
+    personnel_info = crud.get_personnel_info_by_id(db, personnel_id)
     if personnel_info is None:
-        personnel_info = schemas.enlisted_personnel.EnlistedPersonnelRead(success=False, message="Personnel info not found")
+        personnel_info = schema.EnlistedPersonnelRead(success=False, message="Personnel info not found")
     return personnel_info
 
 
-@router.put("/{personnel_id}", response_model=schemas.enlisted_personnel.EnlistedPersonnelUpdateResult)
-async def update_personnel_info(personnel_id: int, personnel: schemas.enlisted_personnel.EnlistedPersonnelUpdate = Body(), db: Session = Depends(deps.get_db)):
-    if crud.enlisted_personnel.get_personnel_info_by_id(db, personnel_id) is not None:
-        crud.enlisted_personnel.update_personnel_info(db, personnel_id, personnel)
-        res = schemas.enlisted_personnel.EnlistedPersonnelUpdateResult(success=True, message="Personnel info successfully updated")
+@router.put("/{personnel_id}", response_model=schema.EnlistedPersonnelUpdateResult)
+async def update_personnel_info(personnel_id: int, personnel: schema.EnlistedPersonnelUpdate = Body(), db: Session = Depends(deps.get_db)):
+    if crud.get_personnel_info_by_id(db, personnel_id) is not None:
+        crud.update_personnel_info(db, personnel_id, personnel)
+        res = schema.EnlistedPersonnelUpdateResult(success=True, message="Personnel info successfully updated")
     else:
-        res = schemas.enlisted_personnel.EnlistedPersonnelUpdateResult(success=False, message="Personnel info not found")
+        res = schema.EnlistedPersonnelUpdateResult(success=False, message="Personnel info not found")
     return res
