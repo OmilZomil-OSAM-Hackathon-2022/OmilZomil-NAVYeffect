467402bc35cb349fb95da6200e0d30a28237a480
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-06 10:15:22 -0400

    main.py 구조 변경 반영

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-import app.routers.base_access as base_access_router
-import app.routers.enlisted_personnel as enlisted_personnel_router
+from app.api.api import api_router
 
 
 app = FastAPI()
-app.include_router(base_access_router.router)
-app.include_router(enlisted_personnel_router.router)
+app.include_router(api_router)
