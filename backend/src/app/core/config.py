918710e1b3701ce6303f544a919f3a2a0558e182
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-06 09:00:22 -0400

    setting.py 파일명 변경 및 불필요 설정 삭제

---
+++ backend/src/app/core/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+DB_INFO = {
+    "ip": os.environ["MYSQL_HOST"],
+    "port": 3306,
+    "user": os.environ["MYSQL_USER"],
+    "pw": os.environ["MYSQL_PASSWORD"],
+    "name": os.environ["MYSQL_DATABASE"],
+}
