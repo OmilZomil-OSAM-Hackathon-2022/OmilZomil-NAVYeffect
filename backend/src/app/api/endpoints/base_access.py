e1b5902a37bcb3b71c93597dd54dbf3f7ce7464d
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-06 10:37:52 -0400

    base_access init.py 미사용

--- backend/src/app/api/endpoints/base_access.py
+++ backend/src/app/api/endpoints/base_access.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app import crud, schemas
+from app.crud import base_access as crud
+from app.schemas import base_access as schema
 from app.api import deps
 
 
 router = APIRouter()
 
 
-@router.get("/{access_id}", response_model=schemas.base_access.BaseAccessRead)
+@router.get("/{access_id}", response_model=schema.BaseAccessRead)
 async def get_access_log_by_id(access_id: int, db: Session = Depends(deps.get_db)):
-    access_log = crud.base_access.get_access_log_by_id(db, access_id)
+    access_log = crud.get_access_log_by_id(db, access_id)
     if access_log is None:
-        access_log = schemas.base_access.BaseAccessRead(success=False, message="Access log not found")
+        access_log = schema.BaseAccessRead(success=False, message="Access log not found")
     return access_log
