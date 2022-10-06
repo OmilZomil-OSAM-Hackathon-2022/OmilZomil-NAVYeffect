0b06cdaa73da6a8df6f8a5a59f9afc2a9ecf5e4c
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-06 10:51:33 -0400

    엔드포인트 삭제 반영

--- backend/src/app/api/api.py
+++ backend/src/app/api/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
-from app.api.endpoints import base_access, enlisted_personnel
+from app.api.endpoints import enlisted_personnel
 
 
 api_router = APIRouter()
-api_router.include_router(base_access.router, prefix="/access", tags=["부대 출입 정보"])
 api_router.include_router(enlisted_personnel.router, prefix="/enlisted", tags=["장병 복장 정보"])
