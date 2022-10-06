60f1f11b3850576d2728ed5804ea4ed8c2af4a13
HeoTaeRyang <cake0702@naver.com>
2022-10-06 15:26:30 +0000

    리스트업 페이지 관리자 확인 추가

--- frontend/omilzomil/src/components/ListUp.vue
+++ frontend/omilzomil/src/components/ListUp.vue
@@ -8,20 +8,23 @@
         <div style="width:80px">
           사진
         </div>
-        <div style="width:80px;display: flex;justify-content: center;">
+        <div style="width:65px;display: flex;justify-content: center;">
           소속 / 계급 / 이름
         </div>
-        <div style="width:65px">
+        <div style="width:45px">
           시간
         </div>
-        <div style="width:100px">
+        <div style="width:95px">
           복장
         </div>
-        <div style="width:55px">
+        <div style="width:35px">
           두발상태
         </div>
-        <div style="width:55px">
+        <div style="width:10px">
           복장상태
+        </div>
+        <div style="width:30px">
+          관리자 확인
         </div>
         <div style="width:43px;display: flex;justify-content: flex-end;">
           자세히보기
@@ -34,44 +37,47 @@
           class="list-item"
           :style="{gap:gap+'px'}"
         >
-          <img
-            class="thumb"
-            src="@/assets/images/test.png"
-          >
-          <div class="info">
-            <div
-              class="division"
-              :style="{color:getDivisionColor(item.division)}"
+          <div class="left">
+            <img
+              class="thumb"
+              src="@/assets/images/test.png"
             >
-              대한민국 {{ item.division }}
+            <div class="info">
+              <div
+                class="division"
+                :style="{color:getDivisionColor(item.division)}"
+              >
+                대한민국 {{ item.division }}
+              </div>
+              <div class="name">
+                {{ item.uClass }} {{ item.uName }}
+              </div>
             </div>
-            <div class="name">
-              {{ item.uClass }} {{ item.uName }}
+            <div class="time">
+              {{ item.time }}
+            </div>
+            <div class="dress-type">
+              <IconBase
+                :width="16"
+                :height="16"
+                :viewBox="'0 0 16 16'"
+              >
+                <TshirtIcon />
+              </IconBase>
+              {{ item.dressType }}
             </div>
           </div>
-          <div class="time">
-            {{ item.time }}
-          </div>
-          <div class="dress-type">
-            <IconBase
-              :width="16"
-              :height="16"
-              :viewBox="'0 0 16 16'"
+          <div class="right">
+            <GoodBadTag :is-good="item.hairStatus" />
+            <GoodBadTag :is-good="item.dressStatus" />
+            <CheckTag :is-check="item.managerStatus" />
+            <a
+              class="more"
+              @click="openDetail(item)"
             >
-              <TshirtIcon />
-            </IconBase>
-            {{ item.dressType }}
+              <img src="@/assets/icons/dots.svg">
+            </a>
           </div>
-
-          <GoodBadTag :is-good="item.hairStatus" />
-          <GoodBadTag :is-good="item.dressStatus" />
-          
-          <a
-            class="more"
-            @click="openDetail(item)"
-          >
-            <img src="@/assets/icons/dots.svg">
-          </a>
         </div>
       </div>
     </div>
@@ -88,6 +94,7 @@
 import TshirtIcon from "../assets/icons/tshirt-icon.vue";
 import IconBase from "./IconBase.vue";
 import GoodBadTag from "./GoodBadTag.vue";
+import CheckTag from "./CheckTag.vue";
 import DetailCard from "./DetailCard.vue";
 
 class Item{
@@ -99,12 +106,13 @@ class Item{
     this.time= "2020-06-24 22:57:36",
     this.dressType= "해군 전투복",
     this.hairStatus= true,
-    this.dressStatus= false
+    this.dressStatus= false,
+    this.managerStatus=true
   }
 }
 
 export default {
-    components: { TshirtIcon, IconBase, GoodBadTag, DetailCard },
+    components: { TshirtIcon, IconBase, GoodBadTag, DetailCard, CheckTag },
     props:{
       gap: {
         type:String,
@@ -180,37 +188,74 @@ export default {
   display:flex;
   flex-direction: column;
   gap:16px;
+  width: 1066px;
+  height: 80px;
 }
 .list-item{
   display: flex;
-  /* gap:100px; */
+  gap:75px;
   align-items: center;
+  width: 1066px;
+  height: 80px;
 }
-.list-item .thumb{
+.list-item .left{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 90px;
+  width: 581px;
+  height: 80px;
+}
+.left .thumb{
   width:80px;
   height:80px;
-  border-radius: 100%;
+  border-radius: 82px;
 }
-.list-item .info{
-  width:80px;
+.left .info{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+
+  width: 79px;
+  height: 41px;
 }
-.list-item .info .division{
+.left .info .division{
+  width: 72px;
+  height: 14px;
+
   font-family: 'Roboto';
   font-style: normal;
   font-weight: 400;
   font-size: 12px;
   line-height: 14px;
-  /* identical to box height */
 
   letter-spacing: 0.4px;
 
   color: #528EE9;
-  
-  margin-bottom: 8px;
 }
-.list-item .time{
-  width:65px;
+.left .info .name{
+  width: 79px;
+  height: 19px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+
+  letter-spacing: 0.15px;
+
+  color: #585767;
+}
+.left .time{
+  width: 65px;
+  height: 28px;
   word-break: normal;
+
   /* Caption */
 
   font-family: 'Roboto';
@@ -224,15 +269,15 @@ export default {
 
   color: #78798D;
 }
-.list-item .dress-type{
+.left .dress-type{
   display:flex;
   align-items: center;
   justify-content:center;
   box-sizing: border-box;
   /* padding: 4px 5px; */
-  gap: 5px;
+  gap: 3px;
 
-  width: 100px;
+  width: 87px;
   height: 24px;
 
   /* Dark9 */
@@ -249,7 +294,17 @@ export default {
 
   color: #528EE9;
 }
-.list-item .hair-state,.dress-state{
+.list-item .right{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 80px;
+  width: 410px;
+  height: 32px;
+}
+/* .right .hair-state,.dress-state{
   width: 55px;
   height:28px;
   background: var(--color-state-card);
@@ -264,12 +319,12 @@ export default {
   font-size: 14px;
   line-height: 16px;
   letter-spacing: 0.25px;
-}
+} */
 /* .list-item .dress-state{
   width:55px;
   background: var(--color-state-card);
 } */
-.list-item .more{
+.right .more{
   width:32px;
   height: 32px;
   border-radius: 100%;
