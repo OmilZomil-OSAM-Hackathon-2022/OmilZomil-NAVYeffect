085e84691261b6cbd84d0b1573215618553004b2
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-06 10:37:22 -0400

    초기 데이터 세팅 함수 추가

--- backend/src/app/db/init_db.py
+++ backend/src/app/db/init_db.py
@@ -1,7 +1,9 @@
+from sqlalchemy.orm import Session
 from app.db.session import engine
 from app.models.base_access import Base as base_access_model
 from app.models.enlisted_personnel import Base as enlisted_personnel_model
 
 
-base_access_model.metadata.create_all(bind=engine)
-enlisted_personnel_model.metadata.create_all(bind=engine)
+def init_db(db: Session):
+    base_access_model.metadata.create_all(bind=engine)
+    enlisted_personnel_model.metadata.create_all(bind=engine)
