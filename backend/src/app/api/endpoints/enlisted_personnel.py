a5eab7af1592ec0adf131b26ad758c44570eb565
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-06 10:51:53 -0400

    불필요 엔드포인트 삭제

--- backend/src/app/api/endpoints/enlisted_personnel.py
+++ backend/src/app/api/endpoints/enlisted_personnel.py
@@ -8,14 +8,6 @@ from app.api import deps
 router = APIRouter()
 
 
-@router.get("/{personnel_id}", response_model=schema.EnlistedPersonnelRead)
-async def get_personnel_info_by_id(personnel_id: int, db: Session = Depends(deps.get_db)):
-    personnel_info = crud.get_personnel_info_by_id(db, personnel_id)
-    if personnel_info is None:
-        personnel_info = schema.EnlistedPersonnelRead(success=False, message="Personnel info not found")
-    return personnel_info
-
-
 @router.put("/{personnel_id}", response_model=schema.EnlistedPersonnelUpdateResult)
 async def update_personnel_info(personnel_id: int, personnel: schema.EnlistedPersonnelUpdate = Body(), db: Session = Depends(deps.get_db)):
     if crud.get_personnel_info_by_id(db, personnel_id) is not None:
