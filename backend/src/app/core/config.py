cf656595543f594af87eb2e38069e1d6c8f8ad4d
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-06 09:43:36 -0400

    config.py 설정 변경

--- backend/src/app/core/config.py
+++ backend/src/app/core/config.py
@@ -1,10 +1,21 @@
 import os
+import secrets
+from pydantic import BaseSettings
 
 
-DB_INFO = {
-    "ip": os.environ["MYSQL_HOST"],
-    "port": 3306,
-    "user": os.environ["MYSQL_USER"],
-    "pw": os.environ["MYSQL_PASSWORD"],
-    "name": os.environ["MYSQL_DATABASE"],
-}
+class Settings(BaseSettings):
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+    MYSQL_HOST: str = os.environ["MYSQL_HOST"]
+    MYSQL_USER: str = os.environ["MYSQL_USER"]
+    MYSQL_PASSWORD: str = os.environ["MYSQL_PASSWORD"]
+    MYSQL_DB: str = os.environ["MYSQL_DATABASE"]
+    SQLALCHEMY_DATABASE_URI: str = f"mysql+mysqldb://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{3306}/{MYSQL_DB}"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
