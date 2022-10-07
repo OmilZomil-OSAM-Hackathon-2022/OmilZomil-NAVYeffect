69bd32389b5279308cee7b8e0a45d3eed938f4bb
Jun0zo <joon0zo1022@gmail.com>
2022-10-08 04:49:08 +0900

    사람인식모델 수정 (사람만 인식)

--- ai/OZEngine/person_detectors/PersonDetector.py
+++ ai/OZEngine/person_detectors/PersonDetector.py
@@ -20,7 +20,7 @@ class PersonDetector():
         if only_person:
             self.classes = ['person']
         else:
-            with open("OZEngine/person_detectors/names/coco.names", "r") as f:
+            with open("OZEngine/person_detectors/names/coco_finetuned.names", "r") as f:
                 self.classes = [line.strip() for line in f.readlines()]
         self.layer_names = self.net.getLayerNames()
         self.output_layers = [self.layer_names[i-1]
@@ -59,22 +59,20 @@ class PersonDetector():
         indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
         # font = cv2.FONT_HERSHEY_PLAIN
 
-        roi = None
+        x, y, w, h = None, None, None, None
         for i in range(len(boxes)):
             if i in indexes:
-                x, y, w, h = boxes[i]
+                
                 class_id = class_ids[i]
                 if class_id > len(self.classes):
                     continue
-                label = str(self.classes[class_id])
-                color = Color.RED  # self.colors[i]
-                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                else:
+                    x, y, w, h = boxes[i]
+                    break
+                
                 if x < 0:
                     x = 0
                 if y < 0:
                     y = 0
-                roi = org_img[y:y+h, x:x+w]
-                # cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
-
-        plt_imshow("Image", img)
-        return roi, img
+                
+        return ((x,y), (x+w, y+h))
