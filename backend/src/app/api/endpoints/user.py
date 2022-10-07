c891f77092c1457bb01c6d01f9b5addca8ec5e46
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 14:03:38 -0400

    유저 비밀번호 변경 로직 수정 반영

--- backend/src/app/api/endpoints/user.py
+++ backend/src/app/api/endpoints/user.py
@@ -26,12 +26,7 @@ async def update_user_information(user_id: int, information: schema.UserUpdateIn
 
 @router.put("/password/{user_id}", response_model=schema.UserResponse)
 async def update_user_password(user_id: int, password: schema.UserUpdatePassword = Body(), db: Session = Depends(deps.get_db)):
-    res = crud.update_user_password(db, user_id, password)
-    if res is not None:
-        res = schema.UserResponse(success=True, message="success")
-    else:
-        res = schema.UserResponse(success=False, message="entry not found")
-    return res
+    return crud.update_user_password(db, user_id, password)
 
 
 @router.put("/role/{user_id}", response_model=schema.UserResponse)
