5bc6b378f1cd1028db8f316f95229f6abf29d608
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-07 13:01:01 -0400

    검사기록 외래키 객체 참조

--- backend/src/app/models/inspection_log.py
+++ backend/src/app/models/inspection_log.py
@@ -1,16 +1,18 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from app.db.base_schema import Base
+from app.model.access_log import AccessLog
+from app.model.uniform import Uniform
 
 
 class InspectionLog(Base):
     __tablename__ = "inspection_log"
 
     inspection_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    access_id = Column(Integer, ForeignKey("access_log.access_id"), nullable=False)
+    access_id = Column(Integer, ForeignKey(AccessLog.access_id), nullable=False)
     affiliation = Column(String(6), nullable=False)
     name = Column(String(8), nullable=False)
     rank = Column(String(4), nullable=False)
-    uniform = Column(String(3), ForeignKey("uniform.uniform"), nullable=False)
+    uniform = Column(String(3), ForeignKey(Uniform.uniform), nullable=False)
     has_name = Column(Boolean, nullable=False)
     has_rank = Column(Boolean, nullable=False)
     has_neckerchief = Column(Boolean, nullable=False)
