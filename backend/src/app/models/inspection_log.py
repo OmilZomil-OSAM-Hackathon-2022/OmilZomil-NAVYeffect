cfd0be4c6906d4c0cc3d445f928f47b7883e8b4a
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 11:03:12 -0400

    검사기록 DB 구조 변경

--- backend/src/app/models/inspection_log.py
+++ backend/src/app/models/inspection_log.py
@@ -2,15 +2,15 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from app.db.base_schema import Base
 
 
-class EnlistedPersonnel(Base):
-    __tablename__ = "enlisted_personnel"
+class InspectionLog(Base):
+    __tablename__ = "inspection_log"
 
-    personnel_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    inspection_id = Column(Integer, primary_key=True, index=True, nullable=False)
     access_id = Column(Integer, ForeignKey("base_access.access_id"), nullable=False)
-    army_type = Column(String(30), nullable=False)
-    name = Column(String(30), nullable=False)
-    rank = Column(String(30), nullable=False)
-    uniform_type = Column(String(30), nullable=False)
+    affiliation = Column(String(6), nullable=False)
+    name = Column(String(8), nullable=False)
+    rank = Column(String(4), nullable=False)
+    uniform = Column(String(3), ForeignKey("uniform.uniform"), nullable=False)
     has_name = Column(Boolean, nullable=False)
     has_rank = Column(Boolean, nullable=False)
     has_neckerchief = Column(Boolean, nullable=False)
