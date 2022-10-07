9921beedacf0f2bd4af7c5682e625934e372c949
HeoTaeRyang <cake0702@naver.com>
2022-10-07 19:08:40 +0000

    eventcard 제작

---
+++ frontend/omilzomil/src/views/EventCardTest.vue
@@ -0,0 +1,10 @@
+<template>
+    <EventCard name="계룡대 본부대대" contents="충청지역 부대 중 외적 군기 1등을 축하합니다!" />
+</template>
+
+<script>
+    import EventCard from '@/components/EventCard.vue'
+    export default {
+    components: { EventCard }
+}
+</script>
