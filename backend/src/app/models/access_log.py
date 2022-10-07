1140f1daa641e925eccfb99a1aa25af0e76fca59
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-07 10:38:21 -0400

    부대출입정보 불필요 주석 삭제

--- backend/src/app/models/access_log.py
+++ backend/src/app/models/access_log.py
@@ -6,6 +6,6 @@ class BaseAccess(Base):
     __tablename__ = "base_access"
 
     access_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    base_name = Column(String(30), nullable=False)  # 부대명
-    access_time = Column(DateTime, nullable=False)  # 출입 시간
-    image = Column(LargeBinary, nullable=False)  # 출입 사진
+    base_name = Column(String(30), nullable=False)
+    access_time = Column(DateTime, nullable=False)
+    image = Column(LargeBinary, nullable=False)
