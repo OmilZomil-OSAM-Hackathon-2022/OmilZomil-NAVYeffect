105ffacc4c6f802ec5be5d59c6ef823541e98629
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 12:56:33 -0400

    검사기록 외래키 설정오류 해결

--- backend/src/app/models/inspection_log.py
+++ backend/src/app/models/inspection_log.py
@@ -6,7 +6,7 @@ class InspectionLog(Base):
     __tablename__ = "inspection_log"
 
     inspection_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    access_id = Column(Integer, ForeignKey("base_access.access_id"), nullable=False)
+    access_id = Column(Integer, ForeignKey("access_log.access_id"), nullable=False)
     affiliation = Column(String(6), nullable=False)
     name = Column(String(8), nullable=False)
     rank = Column(String(4), nullable=False)
