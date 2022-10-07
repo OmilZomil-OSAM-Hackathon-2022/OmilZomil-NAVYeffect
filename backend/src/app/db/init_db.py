bb7c78dba1dbbbed6eafbc25e52f9fb1b67c36c7
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-07 07:16:43 -0400

    군소속 초기 데이터 세팅

--- backend/src/app/db/init_db.py
+++ backend/src/app/db/init_db.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
 from app.db.session import engine
 from app.models.rank import Base as rank_model
+from app.models.affiliation import Base as affiliation_model
 from app.models.military_unit import Base as military_unit_model
 from app.models.base_access import Base as base_access_model
 from app.models.enlisted_personnel import Base as enlisted_personnel_model
 from app.crud import rank as rank_crud
+from app.crud import affiliation as affiliation_crud
 
 
 def init_db(db: Session):
@@ -13,6 +15,13 @@ def init_db(db: Session):
     rank_crud.create_rank(db, "일등병")
     rank_crud.create_rank(db, "상등병")
     rank_crud.create_rank(db, "병장")
+
+    affiliation_model.metadata.create_all(bind=engine)
+    affiliation_crud.create_affiliation(db, "육군")
+    affiliation_crud.create_affiliation(db, "해군")
+    affiliation_crud.create_affiliation(db, "공군")
+    affiliation_crud.create_affiliation(db, "해병대")
+    affiliation_crud.create_affiliation(db, "국방부직할")
 
     military_unit_model.metadata.create_all(bind=engine)
 
