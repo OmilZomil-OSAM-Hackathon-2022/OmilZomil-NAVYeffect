0d21526c67c4570978a27227af51609a9e812c42
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-07 10:39:21 -0400

    부대출입정보 테이블명 및 변수명 변경

--- backend/src/app/models/access_log.py
+++ backend/src/app/models/access_log.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, Integer, String, DateTime, LargeBinary
 from app.db.base_schema import Base
 
 
-class BaseAccess(Base):
-    __tablename__ = "base_access"
+class AccessLog(Base):
+    __tablename__ = "access_log"
 
     access_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    base_name = Column(String(30), nullable=False)
+    military_base = Column(String(30), nullable=False)
     access_time = Column(DateTime, nullable=False)
     image = Column(LargeBinary, nullable=False)
