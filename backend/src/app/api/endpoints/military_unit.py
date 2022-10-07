697d19496193d3de6afdf9d0201c27e89b0b4c89
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-07 13:34:13 -0400

    부대정보 crud 변경사항 적용

--- backend/src/app/api/endpoints/military_unit.py
+++ backend/src/app/api/endpoints/military_unit.py
@@ -11,11 +11,7 @@ router = APIRouter()
 
 @router.post("/", response_model=schema.MilitaryUnitResponse)
 async def create_military_unit(unit: schema.MilitaryUnitCreate = Body(), db: Session = Depends(deps.get_db)):
-    if crud.create_military_unit(db, unit.unit) is not None:
-        res = schema.MilitaryUnitResponse(success=True, message="success")
-    else:
-        res = schema.MilitaryUnitResponse(success=False, message="duplicate entry")
-    return res
+    return crud.create_military_unit(db, unit.unit)
 
 
 @router.get("/", response_model=List[schema.MilitaryUnitRead])
@@ -25,20 +21,9 @@ def get_military_unit(db: Session = Depends(deps.get_db)):
 
 @router.put("/{unit}", response_model=schema.MilitaryUnitResponse)
 async def update_military_unit(unit: str, new_unit: schema.MilitaryUnitUpdate = Body(), db: Session = Depends(deps.get_db)):
-    res = crud.update_military_unit(db, unit, new_unit.unit)
-    if res:
-        res = schema.MilitaryUnitResponse(success=True, message="success")
-    elif res is not None:
-        res = schema.MilitaryUnitResponse(success=False, message="entry not found")
-    else:
-        res = schema.MilitaryUnitResponse(success=False, message="duplicate entry")
-    return res
+    return crud.update_military_unit(db, unit, new_unit.unit)
 
 
 @router.delete("/{unit}", response_model=schema.MilitaryUnitResponse)
 def delete_military_unit(unit: str, db: Session = Depends(deps.get_db)):
-    if crud.delete_military_unit(db, unit):
-        res = schema.MilitaryUnitResponse(success=True, message="success")
-    else:
-        res = schema.MilitaryUnitResponse(success=False, message="entry not found")
-    return res
+    return crud.delete_military_unit(db, unit)
