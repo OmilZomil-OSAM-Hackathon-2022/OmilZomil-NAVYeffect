1654652c904e1cf9cf7fb6e8e6b20ad50bd37575
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 12:57:31 -0400

    유저 DB 설계

--- backend/src/app/models/user.py
+++ backend/src/app/models/user.py
@@ -1,5 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.base_schema import Base
+from app.models.affiliation import Affiliation
+from app.models.military_unit import MilitaryUnit
+from app.models.rank import Rank
+from app.models.role import Role
 
 
 class User(Base):
@@ -8,9 +12,9 @@ class User(Base):
     user_id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String(8), nullable=False)
     dog_number = Column(String(16), unique=True, nullable=False)
-    affiliation = Column(String(6), ForeignKey("affiliation.affiliation"), nullable=False)
-    military_unit = Column(String(128), ForeignKey("military_unit.unit"), nullable=False)
-    rank = Column(String(4), ForeignKey("rank.rank"), nullable=False)
+    affiliation = Column(String(6), ForeignKey(Affiliation.affiliation), nullable=False)
+    military_unit = Column(String(128), ForeignKey(MilitaryUnit.unit), nullable=False)
+    rank = Column(String(4), ForeignKey(Rank.rank), nullable=False)
     username = Column(String(17), unique=True, nullable=False)
     password = Column(String(17), nullable=False)
-    role = Column(String(6), ForeignKey("role.role"), default="unauthorized")
+    role = Column(String(6), ForeignKey(Role.role), default="unauthorized")
