9f47e727c0c33990b40f45cb2b5afeb10bdb57a0
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 10:47:20 -0400

    부대출입정보 crud 변경

--- backend/src/app/crud/access_log.py
+++ backend/src/app/crud/access_log.py
@@ -1,15 +1,15 @@
 from sqlalchemy.orm import Session
-from app.models.base_access import BaseAccess
-from app.schemas.base_access import BaseAccessCreate
+from app.models.access_log import AccessLog
+from app.schemas.access_log import AccessLogCreate
 
 
 def get_access_log_by_id(db: Session, access_id: int):
-    return db.query(BaseAccess).get(access_id)
+    return db.query(AccessLog).get(access_id)
 
 
-def create_access_log(db: Session, access: BaseAccessCreate):
-    base_access = BaseAccess(base_name=access.base_name, access_time=access.access_time, image=access.image)
-    db.add(base_access)
+def create_access_log(db: Session, access: AccessLogCreate):
+    access_log = AccessLog(military_base=access.military_base, access_time=access.access_time, image=access.image)
+    db.add(access_log)
     db.commit()
-    db.refresh(base_access)
-    return base_access
+    db.refresh(access_log)
+    return access_log
