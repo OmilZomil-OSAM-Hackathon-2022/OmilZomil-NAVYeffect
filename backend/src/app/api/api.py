e1c2da5a5d6ff4710e18941eb33cb8e1a837dcb9
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-07 11:26:12 -0400

    api에 엔드포인트 변경 반영

--- backend/src/app/api/api.py
+++ backend/src/app/api/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from app.api.endpoints import military_unit
-from app.api.endpoints import enlisted_personnel
+from app.api.endpoints import inspection_log
 
 
 api_router = APIRouter()
 api_router.include_router(military_unit.router, prefix="/unit", tags=["부대 정보"])
-api_router.include_router(enlisted_personnel.router, prefix="/enlisted", tags=["장병 복장 정보"])
+# api_router.include_router(inspection_log.router, prefix="/enlisted", tags=["장병 복장 정보"])
