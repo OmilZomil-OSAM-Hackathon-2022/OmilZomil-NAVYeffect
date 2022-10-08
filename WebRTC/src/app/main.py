819b155e3b737f278e2bf3fb014d428433b5bb89
greenrain78 <kdwkdw0078@gmail.com>
2022-10-08 12:54:28 +0900

    backend 기본 기능 구현 완료 web socket 추가 완료

--- WebRTC/src/app/main.py
+++ WebRTC/src/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-# from app.api.api import api_router
+from app.api.api import api_router
 
 
 app = FastAPI()
-# app.include_router(api_router)
 
 app.mount("/", StaticFiles(directory="/backend/src/app/static", html = True), name="static")
-# app.mount("/", StaticFiles(directory="/backend/src/app/test", html = True), name="static")
+
+app.include_router(api_router)
+
