44f41c46794cba43eaa5b42dfd0a8630cf3f2642
greenrain78 <kdwkdw0078@gmail.com>
2022-10-08 22:45:03 +0900

    스크립트 수정

--- build.sh
+++ build.sh
@@ -14,9 +14,7 @@ DIR_PATH=`pwd`
 
 # 환경변수 파일 정의
 cat .env.private > .env.lock
-echo CAMERA_DIR_PATH="$DIR_PATH"/WebRTC>> .env.lock
-echo BACKEND_DIR_PATH="$DIR_PATH"/backend >> .env.lock
-echo FRONTEND_DIR_PATH="$DIR_PATH"/frontend >> .env.lock
+echo DIR_PATH="$DIR_PATH" >> .env.lock
 
 # 기존 컨테이너 지우기
 echo [+] remove container
@@ -26,38 +24,41 @@ sudo docker-compose --env-file .env.lock down
 echo [+] docker build
 sudo docker-compose --env-file .env.lock build
 
-
 # DB 실행
 echo [+] make db
 sudo docker-compose --env-file .env.lock up -d db
 
-# DB 테이블 만들기
+# DB 테이블 만들기 - omilzomil backend 참조
 echo [+] make db tables
 sudo docker-compose --env-file .env.lock run --rm web python src/initial_data.py
 
 # 프론트 빌드
 echo [+] frontend build
-sudo docker-compose --env-file .env.lock up -d vue
+sudo docker-compose --env-file .env.lock up -d web_vue
+sudo docker-compose --env-file .env.lock up -d camera_vue
 
-
-# ssl 만들기
-# .env 파일이 있는지 검증
-if [ ! -e "./backend/key.pem" ]; then
-    echo [+] key.pem 파일이 없어 생성합니다.
-    cd ./backend
+# ssl 만들기 - .env 파일이 있는지 검증 => 없으면 생성
+if [ ! -e "./omilzomil/backend/key.pem" ]; then
+    echo [+] omilzomil 에 key.pem 파일이 없어 생성합니다.
+    cd ./omilzomil/backend
+    openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
+fi
+if [ ! -e "./webrtc/backend/key.pem" ]; then
+    echo [+] webrtc 에 key.pem 파일이 없어 생성합니다.
+    cd ./webrtc/backend
     openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 fi
 
+
 # docker 빌드 캐쉬 제거
-# echo [+] remove build cache
-# sudo docker builder prune 
+echo [+] remove build cache
+sudo docker builder prune -f
 
 echo [+] frontend build 대기
 
-until sudo docker-compose --env-file .env.lock ps --services --filter status=stopped | grep -q 'vue'; do
-
+until sudo docker-compose --env-file .env.lock ps --services --filter status=runnig | grep -q 'vue'; do
     wait_time=`date +%T`
-    echo $wait_time
+    echo webrtc $wait_time
     sleep 1;
 done;
 
