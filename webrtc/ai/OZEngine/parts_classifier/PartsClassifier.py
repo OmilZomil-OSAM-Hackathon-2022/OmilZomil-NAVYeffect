18d7e346d6cc68e95e2e57ba293dc6419d9fed8c
Jun0zo <joon0zo1022@gmail.com>
2022-10-10 17:21:56 +0000

    부품분류모델 init.py 추가

--- webrtc/ai/OZEngine/parts_classifier/PartsClassifier.py
+++ webrtc/ai/OZEngine/parts_classifier/PartsClassifier.py
@@ -1,7 +1,6 @@
 import numpy as np
 from OZEngine.parts_classifier import FeatureExtractor
 
-
 class PartsClassifier():
     def __init__(self):
         self.feature_extractor = FeatureExtractor()
