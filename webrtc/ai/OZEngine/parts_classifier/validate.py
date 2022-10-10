01574c613b005dee6861489bd180f78e67352348
Jun0zo <joon0zo1022@gmail.com>
2022-10-10 17:32:30 +0000

    부품인식모델 잘못된 멤버변수 명 수정

--- webrtc/ai/OZEngine/parts_classifier/validate.py
+++ webrtc/ai/OZEngine/parts_classifier/validate.py
@@ -3,4 +3,5 @@ from OZEngine.parts_classifier.PartsClassifier import PartsClassifier
 
 classifier = PartsClassifier()
 img = Image.open("./validation_set/navy_service_uniform/class_tag/4.jpg")
-classifier.classify(img)
+res = classifier.classify(img)
+print(res)
