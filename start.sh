a9986b89fccc52dccc273c73bd08a3f8ccc67738
greenrain78 <kdwkdw0078@gmail.com>
2022-10-12 14:30:04 +0000

    #189 deploy용이랑 dev용을 매겨밴수로 분리 bind와 volumn 의 용어차이를 구분하여 작성

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # 스크립트 설명: 서비스를 시작
 # 재시작시 백앤드에서 코드를 수정하면 반영이 되지만 프론트 앤드는 반영이 안되어서 build.sh부터 실행시켜줘야함
 
+DIR_PATH=`pwd`
 
 input=$1
 if [ "$input" = "--build" ]; then
@@ -21,7 +22,14 @@ if [ "$input" = "--build" ]; then
         sleep 1;
     done;
 
+elif [ "$input" = "--SERVER" ]; then
+    echo [+] run web camera
+    sudo docker-compose --env-file .env.private up web camera
+
+else
+    echo [+] run web camera
+    sudo docker-compose --env-file .env.lock up web camera
+
 fi
 
-echo [+] run web camera
-sudo docker-compose --env-file .env.lock up web camera
+
