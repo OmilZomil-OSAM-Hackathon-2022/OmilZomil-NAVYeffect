bc0459e31d24614effca3f927c89d8e973656267
greenrain78 <kdwkdw0078@gmail.com>
2022-10-12 15:58:51 +0000

    start 스크립트 충돌 해결

--- start.sh
+++ start.sh
@@ -30,33 +30,6 @@ else
     echo [+] run web camera
     sudo docker-compose --env-file .env.lock up web camera
 
-<<<<<<< HEAD
-input=$1
-if [ "$input" = "--build" ]; then
-    echo [+] frontend build
-    sudo docker-compose --env-file .env.lock up web_vue
-    sudo docker-compose --env-file .env.lock up camera_vue
-
-    echo [+] frontend build 대기
-
-    while sudo docker-compose --env-file .env.lock ps --services --filter status=running | grep -q 'vue'; do
-        echo `sudo docker-compose --env-file .env.lock ps --services --filter status=running`
-        wait_time=`date +%T`
-        echo frontend $wait_time
-        sleep 1;
-    done;
-fi
-
-echo [+] Checking build files...
-if [ ! -f ./omilzomil/frontend/dist/index.html ] ; then
-    echo "[!] Please run 'build.sh' first! - omilzomil front NONE"
-    exit
-fi
-if [ ! -f ./webrtc/frontend/dist/index.html ] ; then
-    echo "[!] Please run 'build.sh' first! - webrtc front NONE"
-    exit
-=======
->>>>>>> dev/hotfix2
 fi
 
 
