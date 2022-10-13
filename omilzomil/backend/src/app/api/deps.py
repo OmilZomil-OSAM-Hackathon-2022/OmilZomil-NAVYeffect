8b6845156ee5353b65ef7b8f5acdd3ceb281fa06
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 12:10:46 -0400

    dependecy 함수 셀계

--- omilzomil/backend/src/app/api/deps.py
+++ omilzomil/backend/src/app/api/deps.py
@@ -1,5 +1,18 @@
 from typing import Generator
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+from app.core import security
+from app.core.config import settings
+from app.models.user import Base as user_model
+from app.crud import user as user_crud
+from app.schemas import user as user_schema
 from app.db.session import SessionLocal
+
+
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/login/access-token")
 
 
 def get_db() -> Generator:
@@ -8,3 +21,30 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)) -> user_model:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
+        token_data = user_schema.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = user_crud.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+def get_current_active_user(current_user: user_model = Depends(get_current_user)) -> user_model:
+    if not user_crud.is_active(current_user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def get_current_active_superuser(current_user: user_model = Depends(get_current_user)) -> user_model:
+    if not user_crud.is_superuser(current_user):
+        raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
+    return current_user
