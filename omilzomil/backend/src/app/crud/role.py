e1185865c0ee3d33dde0138e432a221b3e6574d8
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 09:59:10 -0400

    권한 CRUD 표기 오류 수정

--- omilzomil/backend/src/app/crud/role.py
+++ omilzomil/backend/src/app/crud/role.py
@@ -3,7 +3,7 @@ from app.models.role import Role
 
 
 def create_role(db: Session, role_id: int, role: str):
-    role = Role(rold_id=role_id, role=role)
+    role = Role(role_id=role_id, role=role)
     db.add(role)
     db.commit()
     db.refresh(role)
