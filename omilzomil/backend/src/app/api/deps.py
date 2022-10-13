2eaee2473385afdac42c7e898f759d07b283d455
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 13:11:11 -0400

    JWT 확인 기능 추가

--- omilzomil/backend/src/app/api/deps.py
+++ omilzomil/backend/src/app/api/deps.py
@@ -1,14 +1,13 @@
 from typing import Generator
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 from app.core import security
 from app.core.config import settings
-from app.models.user import Base as user_model
-from app.crud import user as user_crud
-from app.schemas import user as user_schema
+from app.crud import user as crud
+from app.schemas.user import UserReadResponse
 from app.db.session import SessionLocal
 
 
@@ -23,28 +22,38 @@ def get_db() -> Generator:
         db.close()
 
 
-def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)) -> user_model:
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
-        token_data = user_schema.TokenPayload(**payload)
+        user_id = payload["sub"]
     except (jwt.JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
-    user = user_crud.get(db, id=token_data.sub)
+        return UserReadResponse(success=False, message="could not validate credentials")
+    user = crud.get_user_by_id(db, user_id=user_id)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        return UserReadResponse(success=False, message="user not found")
+
+    user = UserReadResponse(
+        success=True,
+        message="success",
+        user_id=user.user_id,
+        full_name=user.full_name,
+        dog_number=user.dog_number,
+        affiliation=user.affiliation,
+        military_unit=user.military_unit,
+        rank=user.rank,
+        username=user.username,
+        role=user.role,
+    )
     return user
 
 
-def get_current_active_user(current_user: user_model = Depends(get_current_user)) -> user_model:
-    if not user_crud.is_active(current_user):
+def get_current_active_user(current_user: UserReadResponse = Depends(get_current_user)):
+    if not crud.is_active(current_user):
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
-def get_current_active_superuser(current_user: user_model = Depends(get_current_user)) -> user_model:
-    if not user_crud.is_superuser(current_user):
+def get_current_active_superuser(current_user: UserReadResponse = Depends(get_current_user)):
+    if not crud.is_superuser(current_user):
         raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
     return current_user
