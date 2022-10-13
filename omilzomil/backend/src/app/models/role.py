ce51a45bebbe316d7a640f9382d788dd1ec0e620
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 13:16:04 -0400

    권한 pk명 변경

--- omilzomil/backend/src/app/models/role.py
+++ omilzomil/backend/src/app/models/role.py
@@ -5,5 +5,5 @@ from app.db.base_schema import Base
 class Role(Base):
     __tablename__ = "role"
 
-    role_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     role = Column(String(9), unique=True, nullable=False)
