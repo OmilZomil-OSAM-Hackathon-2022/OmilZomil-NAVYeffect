62db1635a0e2f0f624bd4f1f6e9141be65cddec3
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 13:59:26 -0400

    활성계정 및 슈퍼유저 계정 확인 기능 추가

--- omilzomil/backend/src/app/api/deps.py
+++ omilzomil/backend/src/app/api/deps.py
@@ -1,5 +1,5 @@
 from typing import Generator
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from pydantic import ValidationError
@@ -48,12 +48,12 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusabl
 
 
 def get_current_active_user(current_user: UserReadResponse = Depends(get_current_user)):
-    if not crud.is_active(current_user):
-        raise HTTPException(status_code=400, detail="Inactive user")
+    if current_user.role == "inactive":
+        return UserReadResponse(success=False, message="inactive user")
     return current_user
 
 
 def get_current_active_superuser(current_user: UserReadResponse = Depends(get_current_user)):
-    if not crud.is_superuser(current_user):
-        raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
+    if current_user.role != "super":
+        return UserReadResponse(success=False, message="not enough privileges")
     return current_user
