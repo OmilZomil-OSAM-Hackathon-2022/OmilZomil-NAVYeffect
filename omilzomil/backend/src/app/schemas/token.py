a5f546e7cbba3d83066fbd96321d70fe1b6c4454
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 13:10:40 -0400

    토큰 스키마 불필요 스키마 제거

--- omilzomil/backend/src/app/schemas/token.py
+++ omilzomil/backend/src/app/schemas/token.py
@@ -1,12 +1,7 @@
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import Field
 from app.db.base_schema import Response
-from app.schemas.user import UserRead
 
 
 class TokenResponse(Response):
     access_token: Optional[str] = Field(None, description="access token")
-
-
-class TokenPayload(BaseModel):
-    sub: Optional[int] = Field(None, description="sub")
