39d1b72401d316981ac8bcba502f4ab23a01f44f
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-13 23:19:07 -0400

    부대관리기능 변수명 구체화

--- omilzomil/backend/src/app/api/endpoints/military_unit.py
+++ omilzomil/backend/src/app/api/endpoints/military_unit.py
@@ -19,11 +19,11 @@ def get_military_unit(db: Session = Depends(deps.get_db)):
     return crud.get_military_unit(db)
 
 
-@router.put("/{unit}", response_model=schema.MilitaryUnitResponse)
-async def update_military_unit(unit: int, new_unit: schema.MilitaryUnitUpdate = Body(), db: Session = Depends(deps.get_db)):
-    return crud.update_military_unit(db, unit, new_unit.unit)
+@router.put("/{unit_id}", response_model=schema.MilitaryUnitResponse)
+async def update_military_unit(unit_id: int, new_unit: schema.MilitaryUnitUpdate = Body(), db: Session = Depends(deps.get_db)):
+    return crud.update_military_unit(db, unit_id, new_unit.unit)
 
 
-@router.delete("/{unit}", response_model=schema.MilitaryUnitResponse)
-def delete_military_unit(unit: int, db: Session = Depends(deps.get_db)):
-    return crud.delete_military_unit(db, unit)
+@router.delete("/{unit_id}", response_model=schema.MilitaryUnitResponse)
+def delete_military_unit(unit_id: int, db: Session = Depends(deps.get_db)):
+    return crud.delete_military_unit(db, unit_id)
