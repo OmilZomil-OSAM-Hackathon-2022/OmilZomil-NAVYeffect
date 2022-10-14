33958b2a0d0dcc0fcf41a2eba7cf9b3373761c41
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-14 16:24:46 -0400

    검사기록 세부 모델 설계

---
+++ omilzomil/backend/src/app/models/inspection_detail.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from app.db.base_schema import Base
+from app.models.inspection_log import InspectionLog
+from app.models.appearance import Appearance
+
+
+class InspectionDetail(Base):
+    __tablename__ = "inspection_detail"
+
+    detail_id = Column(Integer, primary_key=True, index=True)
+    inspection_id = Column(Integer, ForeignKey(InspectionLog.inspection_id), nullable=False)
+    appearance_type = Column(Integer, ForeignKey(Appearance.appearance_id), nullable=False)
+    status = Column(Boolean, nullable=False)
+    image_path = Column(String(1024), unique=True, nullable=False)
