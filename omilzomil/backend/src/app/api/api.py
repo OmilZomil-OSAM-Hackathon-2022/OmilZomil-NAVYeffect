f468af35063c5354a24a158741d104578f95ccf7
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-13 23:58:19 -0400

    엔드포인트 설명 구체화

--- omilzomil/backend/src/app/api/api.py
+++ omilzomil/backend/src/app/api/api.py
@@ -5,6 +5,6 @@ from app.api.endpoints import login
 
 
 api_router = APIRouter()
-api_router.include_router(military_unit.router, prefix="/unit", tags=["부대 정보"])
-api_router.include_router(user.router, prefix="/user", tags=["유저 정보"])
+api_router.include_router(military_unit.router, prefix="/unit", tags=["부대 관리"])
+api_router.include_router(user.router, prefix="/user", tags=["유저 관리"])
 api_router.include_router(login.router, prefix="/login", tags=["로그인 기능"])
