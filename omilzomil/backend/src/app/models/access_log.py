fd4022749ff5bbec53003fb3b99c9f4b11598275
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-14 16:08:22 -0400

    출입기록 모델 변경

--- omilzomil/backend/src/app/models/access_log.py
+++ omilzomil/backend/src/app/models/access_log.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime, LargeBinary
+from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
 from app.db.base_schema import Base
+from app.models.military_unit import MilitaryUnit
 
 
 class AccessLog(Base):
     __tablename__ = "access_log"
 
-    access_id = Column(Integer, primary_key=True, index=True, nullable=False)
-    military_base = Column(String(30), nullable=False)
+    access_id = Column(Integer, primary_key=True, index=True)
+    military_unit = Column(Integer, ForeignKey(MilitaryUnit.unit_id), nullable=False)
     access_time = Column(DateTime, nullable=False)
-    image = Column(LargeBinary, nullable=False)
+    image = Column(String(1024), nullable=False)
