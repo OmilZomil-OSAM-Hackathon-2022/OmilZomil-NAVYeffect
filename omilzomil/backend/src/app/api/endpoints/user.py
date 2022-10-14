6bfc3e95afa8ec74ce324cca387698b2f84368d3
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-14 06:08:48 -0400

    유저 엔드포인트 변경

--- omilzomil/backend/src/app/api/endpoints/user.py
+++ omilzomil/backend/src/app/api/endpoints/user.py
@@ -17,19 +17,14 @@ async def create_user(user: schema.UserCreate = Body(), db: Session = Depends(de
 @router.get("/", response_model=List[schema.UserRead])
 def get_users(
     full_name: Optional[str] = None,
-    affiliation: Optional[str] = None,
-    military_unit: Optional[str] = None,
-    rank: Optional[str] = None,
+    affiliation: Optional[int] = None,
+    military_unit: Optional[int] = None,
+    rank: Optional[int] = None,
     is_active: Optional[bool] = None,
     db: Session = Depends(deps.get_db),
 ):
     flt = schema.UserFilter(full_name=full_name, affiliation=affiliation, military_unit=military_unit, rank=rank, is_active=is_active)
     return crud.get_users(db, flt)
-
-
-@router.get("/{user_id}", response_model=schema.UserReadResponse)
-def get_user(user_id: int, db: Session = Depends(deps.get_db)):
-    return crud.get_user(db, user_id)
 
 
 @router.put("/information/{user_id}", response_model=schema.UserResponse)
@@ -45,3 +40,8 @@ async def update_user_password(user_id: int, password: schema.UserUpdatePassword
 @router.put("/role/{user_id}", response_model=schema.UserResponse)
 async def update_user_role(user_id: int, role: schema.UserUpdateRole = Body(), db: Session = Depends(deps.get_db)):
     return crud.update_user_role(db, user_id, role)
+
+
+@router.put("/activity/{user_id}", response_model=schema.UserResponse)
+async def update_user_activity(user_id: int, is_active: schema.UserUpdateActivity = Body(), db: Session = Depends(deps.get_db)):
+    return crud.update_user_activity(db, user_id, is_active)
