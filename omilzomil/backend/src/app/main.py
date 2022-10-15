23a0c1415f229b0d5307dbba9874d0afd04aca51
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-15 09:54:56 -0400

    CORS 설정 main에 추가

--- omilzomil/backend/src/app/main.py
+++ omilzomil/backend/src/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.api.api import api_router
+from app.core.config import settings
 
 
 app = FastAPI()
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 app.include_router(api_router)
 app.mount("/", StaticFiles(directory="/backend/src/app/static", html=True), name="static")
