e13ae27536f2bfe6dd183a3e7ff2f29484e24941
biyotteu <tjqtjq0516@gmail.com>
2022-10-16 00:54:09 +0900

    cors 해결

--- omilzomil/frontend/src/main.js
+++ omilzomil/frontend/src/main.js
@@ -14,22 +14,7 @@ import {
   
 // Vue
 const app = createApp(App)
-// app.config.globalProperties.$axios = axios.create({
-//   timeout: 3000,
-//   headers: {
-//     // 헤더 세팅
-//   },
-//   proxy: {
-//     // url 리소스를 추가해주자
-//     "/": {
-//       // 해당 리소스가 있는 url일 경우 타겟으로 baseURL을 변경
-//       target: "https://127.0.0.1:8888",
-//       // 기본 베이스URL을 바꿔줄지 여부
-//       changeOrigin: true
-//     },
-//   }
-// })
-axios.defaults.baseURL = 'https://127.0.0.1:8888';
+axios.defaults.baseURL = 'https://127.0.0.1:8080';
 app.config.globalProperties.$axios = axios; 
 app.use(store)
 app.use(VueApexCharts)
