6e572f13489c5566e745fe08c7c0979a0c3fc264
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-16 11:47:17 -0400

    통계용 테스트데이터 생성 CRUD 추가

--- omilzomil/backend/src/app/crud/statistics.py
+++ omilzomil/backend/src/app/crud/statistics.py
@@ -7,6 +7,57 @@ from app.models.inspection_detail import InspectionDetail
 from app.crud.military_unit import get_military_unit, get_military_units
 
 
+def create_test_case(db: Session):
+    from string import ascii_letters
+    from random import randrange, choice
+    from datetime import timedelta
+    from dateutil.relativedelta import relativedelta
+
+    ids = [unit.unit_id for unit in get_military_units(db)]
+    if not ids:
+        return {"success": False, "message": "military unit not found"}
+
+    for i in range(0, 10000):
+        access_time = datetime.now() - relativedelta(months=randrange(0, 12)) + timedelta(days=randrange(-15, 15))
+
+        base = AccessLog(military_unit=ids[randrange(0, len(ids))], access_time=access_time)
+        db.add(base)
+        db.commit()
+        db.refresh(base)
+
+        base = InspectionLog(
+            access_id=base.access_id,
+            affiliation=randrange(1, 6),
+            rank=randrange(1, 6),
+            name="".join(choice(ascii_letters) for i in range(4)),
+            uniform=randrange(1, 5),
+            image_path=f"path{i}",
+        )
+        db.add(base)
+        db.commit()
+        db.refresh(base)
+
+        appearance = randrange(0, 10) < 7
+        for j in range(0, 7):
+            if j == 0:
+                status = randrange(0, 10) < 7
+            else:
+                if appearance:
+                    status = True
+                else:
+                    status = bool(randrange(0, 2))
+
+            log = InspectionDetail(
+                inspection_id=base.inspection_id,
+                appearance_type=j + 1,
+                status=status,
+                image_path=f"path{i}",
+            )
+            db.add(log)
+            db.commit()
+            db.refresh(log)
+
+
 def get_monthly_overall_stats(db: Session, military_unit: int = None, date: datetime = None, category: str = None, status: bool = None):
     if date is None:
         date = datetime.now()
