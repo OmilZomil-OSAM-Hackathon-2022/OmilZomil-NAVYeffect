7aa6dc9598fd50fd104698b9577241fa8112f2dc
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-16 04:42:24 -0400

    부대순위 CRUD 추가

--- omilzomil/backend/src/app/crud/statistics.py
+++ omilzomil/backend/src/app/crud/statistics.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.models.access_log import AccessLog
 from app.models.inspection_log import InspectionLog
 from app.models.inspection_detail import InspectionDetail
+from app.crud.military_unit import get_military_unit, get_military_units
 
 
 def get_monthly_overall_stats(db: Session, military_unit: int = None, date: datetime = None, category: str = None, status: bool = None):
@@ -32,7 +33,7 @@ def get_monthly_overall_stats(db: Session, military_unit: int = None, date: date
     else:
         count = total
 
-    return count
+    return (total, count)
 
 
 def get_monthly_detailed_stats(db: Session, appearance_type: int, military_unit: int = None, date: datetime = None, status: bool = None):
@@ -55,3 +56,48 @@ def get_monthly_detailed_stats(db: Session, appearance_type: int, military_unit:
         count = log.count()
 
     return count
+
+
+def take_fourth(elem):
+    return elem[3]
+
+
+def get_monthly_unit_ranks(db: Session):
+    entries = list()
+    for unit in get_military_units(db):
+        total, count = get_monthly_overall_stats(db, military_unit=unit.unit_id, status=True)
+        if total == 0:
+            rate = 0
+        else:
+            rate = round(count / total * 100)
+        entries.append([unit.unit, count, total - count, rate])
+    entries.sort(key=take_fourth, reverse=True)
+
+    ret = list()
+    for i in range(0, len(entries)):
+        entry = {
+            "rank": i + 1,
+            "unit": entries[i][0],
+            "pass": entries[i][1],
+            "fail": entries[i][2],
+            "pass_rate": entries[i][3],
+        }
+        ret.append(entry)
+
+    return ret
+
+
+def get_monthly_best_stats(db: Session, military_unit: int, category: str):
+    ret = dict()
+
+    if category == "unit":
+        res = get_monthly_unit_ranks(db)
+        unit = get_military_unit(db, military_unit)
+        for i in range(0, len(res)):
+            if res[i]["unit"] == unit.unit:
+                ret = {"unit": unit.unit, "rank": res[i]["rank"]}
+                break
+    elif category == "person":
+        pass
+
+    return ret
