1d551a8549488a61610b2fb98509868984e39b04
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-16 09:06:51 -0400

    군소속 데이터길이 변경

--- omilzomil/backend/src/app/models/affiliation.py
+++ omilzomil/backend/src/app/models/affiliation.py
@@ -5,5 +5,5 @@ from app.db.base_schema import Base
 class Affiliation(Base):
     __tablename__ = "affiliation"
 
-    affiliation_id = Column(Integer, primary_key=True, index=True)
-    affiliation = Column(String(6), unique=True, nullable=False)
+    affiliation_id = Column(Integer, primary_key=True)
+    affiliation = Column(String(8), unique=True, nullable=False)
