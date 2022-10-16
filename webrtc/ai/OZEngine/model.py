4da47d807a9614a1c6e289e37dc88a4ebacd8622
Jun0zo <joon0zo1022@gmail.com>
2022-10-16 04:43:33 +0000

    얼굴, 상체 없을 때 None값 예외처리 (형식에 맞춰서)

--- webrtc/ai/OZEngine/model.py
+++ webrtc/ai/OZEngine/model.py
@@ -78,13 +78,13 @@ class OmilZomil:
         person_base_point = person_box[0]
         person_img = box2img(img, person_box)
         if person_img is None:
-            return None
+            return None, None
         
         # 얼굴인식
         face_box = self.face_detector.detect(person_img)
         if face_box is None:
-            return None
-            
+            return None, None
+
         face_img = box2img(person_img, face_box)
 
         self.debug({'face':face_img}, msg='roi')
