e33a4503d0b82158db5f4dc4cc2713ed6cd48d6d
greenrain78 <kdwkdw0078@gmail.com>
2022-10-16 13:35:42 +0000

    webrtc 정상 동작 확인

--- webrtc/backend/src/app/main.py
+++ webrtc/backend/src/app/main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("./app/ai/.")
 sys.path.append("./app/ai/OZEngine/.")
 
+import app.custom_logging
 
 
 from fastapi import FastAPI
@@ -9,19 +10,18 @@ from fastapi.staticfiles import StaticFiles
 
 from app.api.api import api_router
 
-
 app = FastAPI(
     title='omil zomil',
     description="test",
     openapi_url='/api/openapi.json'
 )
+config_path="logging_config.json"
 
+# app.logger = logger
 
 @app.get("/aaaaa")
 def read_root():
     return {'hello': 'world5'}
 
-
 app.include_router(api_router)
-
 app.mount("/", StaticFiles(directory="/backend/src/app/static", html = True), name="static")
