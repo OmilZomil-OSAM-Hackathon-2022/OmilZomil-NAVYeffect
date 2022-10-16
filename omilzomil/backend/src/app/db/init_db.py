7fb6d76cf8341c84793c8c871c77095d92866549
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-16 09:06:22 -0400

    초기데이터에 관리자 추가

--- omilzomil/backend/src/app/db/init_db.py
+++ omilzomil/backend/src/app/db/init_db.py
@@ -12,37 +12,44 @@ from app.models.appearance import Base as appearance_model
 from app.models.inspection_detail import Base as inspection_detail_model
 from app.crud import rank as rank_crud
 from app.crud import affiliation as affiliation_crud
+from app.crud import military_unit as military_unit_crud
 from app.crud import role as role_crud
+from app.crud import user as user_crud
 from app.crud import uniform as uniform_crud
 from app.crud import appearance as appearance_crud
 
 
 def init_db(db: Session):
     rank_model.metadata.create_all(bind=engine)
-    rank_crud.create_rank(db, 1, "이병")
-    rank_crud.create_rank(db, 2, "일병")
-    rank_crud.create_rank(db, 3, "상병")
-    rank_crud.create_rank(db, 4, "병장")
+    rank_crud.create_rank(db, 1, "unknown")
+    rank_crud.create_rank(db, 2, "이병")
+    rank_crud.create_rank(db, 3, "일병")
+    rank_crud.create_rank(db, 4, "상병")
+    rank_crud.create_rank(db, 5, "병장")
 
     affiliation_model.metadata.create_all(bind=engine)
-    affiliation_crud.create_affiliation(db, 1, "육군")
-    affiliation_crud.create_affiliation(db, 2, "해군")
-    affiliation_crud.create_affiliation(db, 3, "공군")
-    affiliation_crud.create_affiliation(db, 4, "해병대")
+    affiliation_crud.create_affiliation(db, 1, "unknown")
+    affiliation_crud.create_affiliation(db, 2, "육군")
+    affiliation_crud.create_affiliation(db, 3, "해군")
+    affiliation_crud.create_affiliation(db, 4, "공군")
+    affiliation_crud.create_affiliation(db, 5, "해병대")
 
     role_model.metadata.create_all(bind=engine)
     role_crud.create_role(db, 1, "user")
     role_crud.create_role(db, 2, "admin")
     role_crud.create_role(db, 3, "super")
 
-    user_model.metadata.create_all(bind=engine)
-
     military_unit_model.metadata.create_all(bind=engine)
+    military_unit_crud.create_military_unit(db, unit_id=1, unit="unknown")
+
+    user_model.metadata.create_all(bind=engine)
+    user_crud.create_super_admin(db, "super", "super")
 
     uniform_model.metadata.create_all(bind=engine)
-    uniform_crud.create_uniform(db, 1, "샘당")
-    uniform_crud.create_uniform(db, 2, "정복")
-    uniform_crud.create_uniform(db, 3, "군복")
+    uniform_crud.create_uniform(db, 1, "unknown")
+    uniform_crud.create_uniform(db, 2, "샘당")
+    uniform_crud.create_uniform(db, 3, "정복")
+    uniform_crud.create_uniform(db, 4, "군복")
 
     access_log_model.metadata.create_all(bind=engine)
     inspection_log_model.metadata.create_all(bind=engine)
