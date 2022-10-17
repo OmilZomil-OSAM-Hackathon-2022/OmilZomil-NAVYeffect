e70d1294f06b29b103d9bd5f3d44ecc4a9964630
biyotteu <tjqtjq0516@gmail.com>
2022-10-18 01:06:02 +0900

    conflict

---
+++ webrtc/ai/OZEngine/dress_checkers/__init__.py
@@ -0,0 +1,13 @@
+print('dress checker')
+
+from os.path import abspath, dirname
+from os import getcwd
+
+import sys
+import os
+
+from OZEngine.lib import utils
+
+from .FullDressUniformChecker import FullDressUniformChecker
+from .NavyServiceUniformChecker import NavyServiceUniformChecker
+from .UniformChecker import UniformChecker
--- webrtc/ai/OZEngine/dress_classifier/DressClassifier.py
+++ webrtc/ai/OZEngine/dress_classifier/DressClassifier.py
@@ -1,11 +1,12 @@
 import numpy as np
 import os
 from OZEngine.FeatureExtractor import FeatureExtractor
-
+from OZEngine.lib.defines import UniformType
 
 class DressClassifier(FeatureExtractor):
     def __init__(self):
-        project_path = '/config/workspace/WEB_CLOUD_OmilZomil_NAVYeffect/webrtc/ai/OZEngine/dress_classifier/Dress'
+        project_path = '/config/workspace/WEB_CLOUD_OmilZomil_NAVYeffect/webrtc/ai/OZEngine/dress_classifier'
+        base_url = os.path.join(project_path, 'Dress')
         super().__init__(base_url)
 
     def predict(self, img):
@@ -13,13 +14,9 @@ class DressClassifier(FeatureExtractor):
         dists = np.linalg.norm(self.features - query, axis=1)
         id = np.argsort(dists)[0]
         dist = dists[id]
-        kind = self.classes[id]
+        kind = self.classes[id].upper()
+        kind = UniformType.dic.get(kind)
         if dist < 1:
             return (dist, kind, id)
         else:
             return (None, None, None)
-
-if __name__ == '__main__':
-    pc = PartsClassifier('navy_service_uniform')
-    res = pc.evaluate()
-    print(res)
