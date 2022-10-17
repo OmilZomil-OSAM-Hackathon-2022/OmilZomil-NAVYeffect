08c1adc34dbc1d9df9b580f55d04d1865fa6a345
greenrain78 <kdwkdw0078@gmail.com>
2022-10-17 11:41:15 +0000

    brocker 구성 완료

---
+++ webrtc/backend/src/app/api/websocket/broker.py
@@ -0,0 +1,48 @@
+import cv2
+from datetime import datetime
+from app.core.config import settings
+import socket
+
+from app.api.websocket.camera import capture, check_human
+
+
+IP, PORT = settings.WORKER_SERVER
+
+class Broker:
+    SAVE_PATH = f"{settings.IMAGE_PATH}/queue"
+
+    def __init__(self, ws, id):
+        self.id = id
+        self.ws = ws
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((IP, PORT))
+
+    async def give_task(self, data):
+        # 사진 분석
+        img = capture(data)
+        result = check_human(img)
+        # 추후 해당 부분 삭제 img
+        result['img'] = data
+        
+    
+        # 사람이 아니면 처리 X
+        if not result['human']:
+            return result
+        
+        # 저장
+        name = datetime.now().strftime("%H-%m-%s")
+        path = f'{self.SAVE_PATH}/{self.id}_{name}.jpg'
+        self.save_img(img, path)
+
+        # task 전송
+        msg = path
+        self.send_task(msg=msg)
+        return result
+
+    def send_task(self, msg):
+        self.socket.sendall(bytes(msg, 'ascii'))
+
+    def save_img(self, img, path):
+        print(path)
+        cv2.imwrite(path, img)
+        print(f"저징 완료 {path}")
