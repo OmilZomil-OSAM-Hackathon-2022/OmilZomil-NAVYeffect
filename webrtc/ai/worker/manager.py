1205929f25d5d16962da6e10c91c40da6bcbeac3
greenrain78 <kdwkdw0078@gmail.com>
2022-10-17 07:30:55 +0000

    worker 구성

--- webrtc/ai/worker/manager.py
+++ webrtc/ai/worker/manager.py
@@ -1,13 +1,29 @@
 
 
 class Manager:
-    pass
+    task_queue = []
+    is_run = True  # 스래드가 많아 더이상 실행 못하는 경우 false
     def __init__(self):
         pass
 
-    def add_task():
-        pass
+    def add_task(self, msg):
+        # 작업 추가
+        self.task_queue.append(msg)
+        # 작업 실행
+        if self.is_run:
+            # 스래드 수에 여유가 있는 경우
+            self.run_task()
+        else:
+            # worker를 생성하지 못하는 경우
+            return False
     
-    def run_task():
-        pass
-    
+    def run_task(self):
+        print("run task")
+        print(self.task_queue)
+        
+        # 작업이 없는 경우는 종료
+        if not self.task_queue:
+            return True
+        
+        # 작업이 없을때까지 반복
+        self.run_task()
--- webrtc/backend/src/app/api/websocket/manager.py
+++ webrtc/backend/src/app/api/websocket/manager.py
@@ -5,16 +5,18 @@ import socket
 
 IP, PORT = settings.WORKER_SERVER
 class Manager:
-    SAVE_PATH = f"{settings.IMAGE_PATH}/queue/"
+    SAVE_PATH = f"{settings.IMAGE_PATH}/queue"
 
     def __init__(self, ws):
         self.ws = ws
 
     def give_task(self, img):
         name = datetime.now().strftime("%H-%m-%s")
-        self.save_img(img, name)
-        msg="asdf"
-        self.send_task(name)
+        path = f'{self.SAVE_PATH}/{name}.jpg'
+        self.save_img(img, path)
+
+        msg = path
+        self.send_task(msg=msg)
 
     def send_task(self, msg):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -22,7 +24,6 @@ class Manager:
             sock.sendall(bytes(msg, 'ascii'))
         pass
 
-    def save_img(self, img, name):
-        path = f'{self.SAVE_PATH}/{name}.jpg'
+    def save_img(self, img, path):
         cv2.imwrite(path, img)
         print(f"저징 완료 {path}")
