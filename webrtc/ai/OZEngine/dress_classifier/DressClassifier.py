03c97c5a34bcc276675618340dd43a482543f22b
Jun0zo <joon0zo1022@gmail.com>
2022-10-17 14:52:39 +0000

    복장분류모델 리턴값 통일 (int값으로)

--- webrtc/ai/OZEngine/dress_classifier/DressClassifier.py
+++ webrtc/ai/OZEngine/dress_classifier/DressClassifier.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from OZEngine.FeatureExtractor import FeatureExtractor
-
+from OZEngine.lib.defines import UniformType
 
 class DressClassifier(FeatureExtractor):
     def __init__(self):
@@ -14,8 +14,8 @@ class DressClassifier(FeatureExtractor):
         dists = np.linalg.norm(self.features - query, axis=1)
         id = np.argsort(dists)[0]
         dist = dists[id]
-        kind = self.classes[id]
-        kind = kind.upper()
+        kind = self.classes[id].upper()
+        kind = UniformType.dic.get(kind)
         if dist < 1:
             return (dist, kind, id)
         else:
