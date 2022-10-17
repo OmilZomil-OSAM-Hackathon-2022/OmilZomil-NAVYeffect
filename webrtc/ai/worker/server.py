ce20229f61f09b9d000b543e4767816c0b70b533
greenrain78 <kdwkdw0078@gmail.com>
2022-10-17 07:30:55 +0000

    worker 구성

--- webrtc/ai/worker/server.py
+++ webrtc/ai/worker/server.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import socketserver
+from worker.manager import Manager
+
+
+manager = Manager()
 
 class TaskHandler(socketserver.BaseRequestHandler):
-    """
-    업무 요청 받음
-    """
-
     def handle(self):
         """
         클라이언트와 연결될 때 호출되는 함수
-        상위 클래스에는 handle() 메서드가 정의되어 있지 않기 때문에
-        여기서 오버라이딩을 해야함
         """
         self.data = self.request.recv(1024).strip()
-        print(f"{self.client_address}")
+        print(f"{self.client_address} = {self.data}")
+        manager.add_task(msg={
+            'socket' : self.request,
+            'data' : self.data,
+        })
