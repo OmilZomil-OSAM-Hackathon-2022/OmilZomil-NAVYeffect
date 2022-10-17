3099797a8aa387320f7dac21bb9633f9ff3061ec
biyotteu <tjqtjq0516@gmail.com>
2022-10-18 00:26:15 +0900

    대쉬보드 백엔드 연결

--- omilzomil/frontend/src/common/jwt.js
+++ omilzomil/frontend/src/common/jwt.js
@@ -1,15 +1,21 @@
+import VueCookies from 'vue-cookies';
+
 const ID_TOKEN_KEY = "id_token"
 
 export const getToken = () => {
-  return window.localStorage.getItem(ID_TOKEN_KEY)
+  // return window.localStorage.getItem(ID_TOKEN_KEY)
+  return VueCookies.get(ID_TOKEN_KEY);
 }
 
 export const saveToken = token => {
-  window.localStorage.setItem(ID_TOKEN_KEY, token)
+  VueCookies.set(ID_TOKEN_KEY,token,'2h');
+  // window.localStorage.setItem(ID_TOKEN_KEY, token)
+
 }
 
 export const destroyToken = () => {
-  window.localStorage.removeItem(ID_TOKEN_KEY)
+  // window.localStorage.removeItem(ID_TOKEN_KEY)
+  VueCookies.remove(ID_TOKEN_KEY);
 }
 
 export default {
