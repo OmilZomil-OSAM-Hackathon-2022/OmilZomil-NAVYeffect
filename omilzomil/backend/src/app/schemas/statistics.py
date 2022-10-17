1b4478f471745a618b5c02b5654dada026badeab
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-17 00:40:40 -0400

    통계목적 일자 지정 불필요 스키마 설계

---
+++ omilzomil/backend/src/app/schemas/statistics.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+class Date(datetime):
+    def __new__(cls, year, month=None, day=None, hour=0, minute=0, second=0, microsecond=0, tzinfo=None, *, fold=0):
+        cls.day = day
+        return super().__new__(cls, year, month, day and day or 1)
+
+    def __repr__(self):
+        L = [self.year, self.month, self.day]
+        return "%s(%s)" % (self.__class__.__qualname__, ", ".join(map(str, L)))
+
+    def __str__(self):
+        return self.strftime(self.day and "%Y-%m-%d" or "%Y-%m")
