dc651389b6745b8d17ee01f5eea3c01312bea5ad
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-18 09:08:21 -0400

    출입 및 기록 통합 모델 설계

--- omilzomil/backend/src/app/models/inspection_log.py
+++ omilzomil/backend/src/app/models/inspection_log.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from app.db.base_schema import Base
-from app.models.access_log import AccessLog
 from app.models.affiliation import Affiliation
+from app.models.guardhouse import Guardhouse
 from app.models.military_unit import MilitaryUnit
 from app.models.rank import Rank
 from app.models.uniform import Uniform
@@ -11,7 +12,8 @@ class InspectionLog(Base):
     __tablename__ = "inspection_log"
 
     inspection_id = Column(Integer, primary_key=True, index=True)
-    access_id = Column(Integer, ForeignKey(AccessLog.access_id), unique=True, nullable=False)
+    guardhouse = Column(Integer, ForeignKey(Guardhouse.house_id), nullable=False)
+    access_time = Column(DateTime, default=datetime.now())
     affiliation = Column(Integer, ForeignKey(Affiliation.affiliation_id), nullable=False)
     military_unit = Column(Integer, ForeignKey(MilitaryUnit.unit_id), default=1)
     rank = Column(Integer, ForeignKey(Rank.rank_id), nullable=False)
