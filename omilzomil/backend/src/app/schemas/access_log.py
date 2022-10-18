2aff0021800e80b72e6520bd0edccc8c952f0d1a
dos-m0nk3y <dosm0nk3y@github.com>
2022-10-18 08:33:48 -0400

    출입기록 스키마 변경

--- omilzomil/backend/src/app/schemas/access_log.py
+++ omilzomil/backend/src/app/schemas/access_log.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
+from app.db.base_schema import Response
 
 
 class AccessLogBase(BaseModel):
-    military_base: str = Field(None, description="military base")
+    guardhouse: str = Field(None, description="guardhouse")
     access_time: datetime = Field(None, description="access time")
-    image_path: bytes = Field(None, description="image path")
 
 
 class AccessLogCreate(AccessLogBase):
@@ -16,9 +16,12 @@ class AccessLogRead(AccessLogBase):
     class Config:
         schema_extra = {
             "example": {
-                "military_base": "계룡대 1정문",
+                "guardhouse": "계룡대 1정문",
                 "access_time": datetime.now(),
-                "image_path": "image path",
             }
         }
         orm_mode = True
+
+
+class AccessLogResponse(Response):
+    pass
