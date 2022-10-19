2785f34bfd10f0dfff812814d129266e17aed97d
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-19 18:06:46 -0400

    실시간 감지 정상/오류 토글 엔드포인트 구현

--- omilzomil/backend/src/app/api/endpoints/real_time_monitor.py
+++ omilzomil/backend/src/app/api/endpoints/real_time_monitor.py
@@ -1,8 +1,9 @@
 from datetime import date
 from typing import Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 from app.schemas.user import UserReadResponse
+from app.schemas.inspection_detail import InspectionDetailUpdateValidity
 from app.crud import real_time_monitor as crud
 from app.api import deps
 
@@ -37,3 +38,16 @@ def get_log_details(
         return {"success": False, "message": current_user.message}
 
     return crud.get_log_details(db, inspection_id)
+
+
+@router.put("/detail/{detail_id}")
+def update_log_detail_validity(
+    detail_id: int,
+    is_valid: InspectionDetailUpdateValidity = Body(),
+    db: Session = Depends(deps.get_db),
+    current_user: UserReadResponse = Depends(deps.get_current_user),
+):
+    if not current_user.success:
+        return {"success": False, "message": current_user.message}
+
+    return crud.update_log_detail_validity(db, detail_id, is_valid)
