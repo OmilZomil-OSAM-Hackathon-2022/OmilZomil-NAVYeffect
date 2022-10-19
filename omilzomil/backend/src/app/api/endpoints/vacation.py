4a7693d1c477c9036f92d75de9c2622660e707ff
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-19 17:53:56 -0400

    휴가장병 검색 엔드포인트 추가

--- omilzomil/backend/src/app/api/endpoints/vacation.py
+++ omilzomil/backend/src/app/api/endpoints/vacation.py
@@ -1,8 +1,10 @@
-from typing import List
+from datetime import datetime
+from typing import List, Optional
 from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 from app.crud import vacation as crud
 from app.schemas.user import UserReadResponse
+from app.schemas.military_unit import MilitaryUnitRead
 from app.schemas import vacation as schema
 from app.api import deps
 
@@ -23,6 +25,17 @@ def get_vacations(user_id: int, db: Session = Depends(deps.get_db)):
 @router.get("/unit/", response_model=List[schema.VacationRead])
 def get_vacations_from_unit(db: Session = Depends(deps.get_db), current_user: UserReadResponse = Depends(deps.get_current_user)):
     return crud.get_vacations(db, unit_id=current_user.military_unit)
+
+
+@router.get("/name/", response_model=List[MilitaryUnitRead])
+def get_unit_names_from_user(
+    access_time: datetime,
+    affiliation: Optional[int] = None,
+    rank: Optional[int] = None,
+    name: Optional[str] = None,
+    db: Session = Depends(deps.get_db),
+):
+    return crud.get_unit_names_from_user(db, access_time=access_time, affiliation=affiliation, rank=rank, name=name)
 
 
 @router.put("/approval/{vacation_id}", response_model=schema.VacationResponse)
