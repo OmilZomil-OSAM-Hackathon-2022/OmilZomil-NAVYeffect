a4c1265e0e4cdaf7758e920601ec1f9b7dd37167
GreenRain <47217640+greenrain78@users.noreply.github.com>
2022-10-19 18:21:19 +0900

    Revert "WebRtc 테스트 용 화면 구성"

--- omilzomil/backend/src/app/models/appearance.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.db.base_schema import Base
-
-
-class Appearance(Base):
-    __tablename__ = "appearance"
-
-    appearance_id = Column(Integer, primary_key=True, index=True)
-    appearance = Column(String(12), unique=True, nullable=False)
