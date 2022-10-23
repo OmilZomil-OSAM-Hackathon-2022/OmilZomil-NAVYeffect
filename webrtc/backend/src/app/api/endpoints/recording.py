a7d4cb4722748fb068536e3bf1051423b11a2772
greenrain78 <kdwkdw0078@gmail.com>
2022-10-23 09:27:37 +0000

    ai 테스트 코드 분리

--- webrtc/backend/src/app/api/endpoints/recording.py
+++ webrtc/backend/src/app/api/endpoints/recording.py
@@ -27,6 +27,66 @@ from app.ai.OZEngine.model import OmilZomil
 router = APIRouter()
 
     
+@router.websocket("/test")
+async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(deps.get_db)):
+    """
+    ai 요구사항만 맞춰서 실행
+    병렬 처리 X
+    버퍼 X - 파일로 저장 X
+    단지 카메라 객체만 유지
+    """
+    # id, time
+    connect_start_time = datetime.now()
+    camera_id = str(uuid.uuid4())
+
+    # 처음 접속 =  위병소 리스트 전달
+    await websocket.accept()
+    house_list = [data.house for data in get_guardhouse(db)]
+    await websocket.send_json({
+        'type' : "list",
+        'list' : house_list,
+    })
+
+    print("소캣 연결 완료")
+    omil_detecter = OmilZomil()
+    
+    # 수신 중
+    print(" 이미지 수신 시작")
+    try:
+        while True:
+            data = await websocket.receive_json()
+            work_start = datetime.now()
+
+            img = photo_2_img(data['photo'])
+            # 임시로 모든 이미지 저장
+            cv2.imwrite(f"./image_list/{camera_id}_{work_start.strftime('%H-%M-%S')}.jpg", img)
+
+            print(f'데이터 수신:- {camera_id}')
+            report = omil_detecter.detect(img)
+            print(report)
+            print(report['step'])
+            print(report.get('component'))
+
+            msg = {
+                'type' : "ai",
+                'step' : report['step'],
+                'component' : report.get('component'),
+            }
+            await websocket.send_json(msg)
+            # 1차 처리 로그 출력
+            print(f'테스크 1차 처리 완료: {camera_id} : {datetime.now() - work_start}')
+            print()
+            print()
+
+
+    except WebSocketDisconnect:
+        print(f'연결 종료: - {camera_id} {datetime.now() - connect_start_time}')
+        pass
+
+    
+
+
+
 @router.websocket("/ai")
 async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(deps.get_db)):
     """
@@ -58,13 +118,18 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(deps.ge
             work_start = datetime.now()
 
             img = photo_2_img(data['photo'])
+            cv2.imwrite(f"./image_list/{camera_id}_{work_start.strftime('%H-%M-%S')}.jpg", img)
 
             print(f'데이터 수신:- {camera_id}')
             report = omil_detecter.detect(img)
-            
+            print(report)
+            print(report['step'])
+            print(report.get('component'))
+
             msg = {
                 'type' : "ai",
-                "report" : report,
+                'step' : report['step'],
+                'component' : report.get('component'),
             }
             await websocket.send_json(msg)
             # 1차 처리 로그 출력
@@ -76,7 +141,6 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(deps.ge
     except WebSocketDisconnect:
         print(f'연결 종료: - {camera_id} {datetime.now() - connect_start_time}')
         pass
-
 
 
     
