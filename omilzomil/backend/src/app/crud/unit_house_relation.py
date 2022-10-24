53b64782bba36407544e612e6b168a1772326976
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2022-10-24 07:59:17 -0400

    위병소 부대 매칭 기능 추가

--- omilzomil/backend/src/app/crud/unit_house_relation.py
+++ omilzomil/backend/src/app/crud/unit_house_relation.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import sqlalchemy.exc
 from sqlalchemy.orm import Session
 from app.models.unit_house_relation import Guardhouse, UnitHouseRelation
 from app.schemas.unit_house_relation import UnitHouseRelationResponse
 from app.crud.military_unit import get_military_unit
 from app.crud.guardhouse import get_guardhouse
+from app.crud.vacation import get_unit_names_from_user
 
 
 def create_unit_house_relation(db: Session, unit_id: int, house_id: int):
@@ -32,6 +34,21 @@ def get_unit_house_relations(db: Session, unit_id: int, house: str = None):
         .order_by(Guardhouse.house)
         .all()
     )
+
+
+def get_unit_from_house(db: Session, house: int, access_time: datetime, affiliation: int = None, rank: int = None, name: str = None):
+    query = db.query(UnitHouseRelation.military_unit).filter_by(guardhouse=house)
+    count = query.count()
+    if count == 0:
+        return 1
+    elif count == 1:
+        return query.first().military_unit
+    else:
+        units = get_unit_names_from_user(db, access_time, affiliation, rank, name)
+        if len(units) == 1:
+            return units[0].unit_id
+        else:
+            return 1
 
 
 def delete_unit_house_relation(db: Session, unit_id: int, house_id: int):
