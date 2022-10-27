ae68e419bbecaef3e71d424197612a0a3330a86c
Jun0zo <joon0zo1022@gmail.com>
2022-10-27 02:05:06 +0000

    demo main.py step적용

--- webrtc/ai/demo/main.py
+++ webrtc/ai/demo/main.py
@@ -10,17 +10,25 @@ import time
 from OZEngine.lib.utils import *
 from OZEngine import OmilZomil
 
-save_path = os.path.join(os.getcwd(), 'image/res/fd_fin_test')
+save_path = os.path.join(os.getcwd(), 'image/res/arm_01')
 os.makedirs(save_path, exist_ok=True)
-model = OmilZomil(debug_list=['imwrite'], save_path=save_path)
+model = OmilZomil()
 
-frame_path = 'image/video_frame/fd_1'
+frame_path = 'image/video_frame/arm01'
 frames = os.listdir(frame_path)
 frame_n = len(frames)
 print('frame n ', frame_n)
-for i in tqdm(range(0, frame_n), desc='detecting'):
+for i in tqdm(range(1, frame_n), desc='detecting'):
     read_path = os.path.join(frame_path, f'{i}.jpg')
     img = cv2.imread(read_path)
     
     result = model.detect(img)
-    # print(result['component'])
+    if result['step'] >= 1:
+        model.saveImg({'result':result['boxed_img']}, save_path=save_path)
+    if result['step'] >= 2:
+        # model.saveImg({'hed_result':result['hed_boxed_img']}, save_path=save_path)
+        model.saveImg({'shirts':result['shirt_img']}, save_path=save_path)
+    
+    if result['step'] >= 3:
+        if result.get('roi'):
+            model.saveImg(result['roi'], save_path=save_path)
